e731977b114fb7fd32c732c2b456f917a0447bef
Roberto Carratala Sanchez <b.rcs@tid.es>
2014-04-29 16:47:02 +0200

    added pre-final version for rpm stage

--- flume/neore/scripts/package.sh
+++ flume/neore/scripts/package.sh
@@ -1,68 +1,93 @@
 #!/bin/bash
 
+#################################################
+###       Main script for Cygnus RPM Stage    ###
+#################################################
+
 function download_flume(){
-	# donwload Vert.x zip form artifactory and unzip it into ${RPM_BASE_DIR}
+	# download form artifactory and unzip it into ${RPM_BASE_DIR}
+	_logStage "######## Preparing the apache-flume component... ########"
+
 	TMP_DIR="tmp_deleteme"
 	mkdir -p ${TMP_DIR}
 	pushd ${TMP_DIR} &> /dev/null
 	FLUME_TAR="apache-flume-1.4.0-bin.tar.gz"
 	FLUME_WO_TAR="apache-flume-1.4.0-bin"
+
+	_log "#### The version of the component is (${FLUME_TAR}) ####"
 	ARTIFACTORY_FLUME_URL="http://artifactory.hi.inet/artifactory/simple/common/flume/"
-	echo -n "Downloading apache-flume: ${FLUME_TAR}... "
+	_log "#### BASE_DIR = $BASE_DIR ####"
+	_log "#### Downloading apache-flume: ${FLUME_TAR}... ####"
 	curl -s -o ${FLUME_TAR} ${ARTIFACTORY_FLUME_URL}/${FLUME_TAR}
 	if [[ $? -ne 0 ]]; then
-			echo "ERROR: cannot download apache-flume.tar.gz (${FLUME_TAR}) from ${ARTIFACTORY_FLUME_URL}"
+			_logError "cannot download apache-flume.tar.gz (${FLUME_TAR}) from ${ARTIFACTORY_FLUME_URL}"
 			return 1
 		else
-			echo "done!"
+			_logOk ".............. Done! .............."
 		fi
 
-		echo -n "Uncompresing apache-flume: ${FLUME_TAR}... "
+	_log "#### Uncompresing apache-flume: ${FLUME_TAR}... ####"
 	tar xvzf ${FLUME_TAR} &> /dev/null
 	if [[ $? -ne 0 ]]; then
-		echo "ERROR: cannot untar flume.tar.gz (${FLUME_TAR})"
+		_logError ".............. Cannot untar flume.tar.gz (${FLUME_TAR}) .............."
 		return 1
 	else
-		echo "done!"
+		_logOk ".............. Done! .............."
 	fi
 
+	_log "#### Cleaning the temporal folders... ####"
 	rm -rf ${RPM_SOURCE_DIR}/${FLUME_WO_TAR}
 	mkdir -p ${RPM_PRODUCT_SOURCE_DIR}
 	cp -R ${FLUME_WO_TAR}/* ${RPM_PRODUCT_SOURCE_DIR}/
 	popd &> /dev/null
 	rm -rf ${TMP_DIR}
 	return 0
+
+	_logStage "######## The apache-flume is ready for use! ... ########"
 }
+
+# Import the colors for deployment script
+source ./colors_shell.sh
+
+_logStage "######## Starting the RPM stage... ########"
+
+_logStage "######## Setting the environment... ########"
 
 # Setting folders this scrtipt is in neore/scripts BASE_DIR. Note: $0/.. is CWD
 BASE_DIR=$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $0/../../..)
-echo "BASE_DIR = $BASE_DIR"
-
-# Create output folder
-# [[ -d "${BASE_DIR}/target" ]] || mkdir -p "${BASE_DIR}/target"
+_log "#### BASE_DIR = $BASE_DIR ####"
 
 RPM_BASE_DIR="${BASE_DIR}/neore/rpm"
-echo "RPM_BASE_DIR = $RPM_BASE_DIR"
-RPM_SOURCE_DIR="${RPM_BASE_DIR}/SOURCES"
-echo "RPM_SOURCE_DIR = $RPM_SOURCE_DIR"
-RPM_PRODUCT_SOURCE_DIR="${RPM_SOURCE_DIR}/usr/cygnus/"
-echo "RPM_PRODUCT_SOURCE_DIR=$RPM_PRODUCT_SOURCE_DIR"
+_log "#### RPM_BASE_DIR = $RPM_BASE_DIR ####"
 
-# Iterate over every SPEC file
+RPM_SOURCE_DIR="${RPM_BASE_DIR}/SOURCES"
+_log "#### RPM_SOURCE_DIR = $RPM_SOURCE_DIR ####"
+
+ RPM_PRODUCT_SOURCE_DIR="${RPM_SOURCE_DIR}/usr/cygnus/"
+_log "#### RPM_PRODUCT_SOURCE_DIR=$RPM_PRODUCT_SOURCE_DIR ####"
+
+_log "#### Creating output folder ####"
+[[ -d "${BASE_DIR}/target" ]] || mkdir -p "${BASE_DIR}/target"
+
+_log "#### Iterate over every SPEC file ####"
 if [[ -d "${RPM_BASE_DIR}" ]]; then
 
 	download_flume
-
+	
 	[[ $? -ne 0 ]] && exit 1
 
+	_logStage "######## Executing the rpmbuild ... ########"
 	for SPEC_FILE in $(find "${RPM_BASE_DIR}" -type f -name *.spec)
 	do
-		echo "Packaging using: ${SPEC_FILE}..."
+		_log "#### Packaging using: ${SPEC_FILE}... ####"
 		# Execute command to create RPM
-		echo "rpmbuild -v -ba ${SPEC_FILE} --define '_topdir '${RPM_BASE_DIR} --define '_product_version 0.1'"
+		_log "#### Rpmbuild -v -ba ${SPEC_FILE} --define '_topdir '${RPM_BASE_DIR} --define '_product_version 0.1' ####"
 		rpmbuild -v -ba ${SPEC_FILE} --define '_topdir '${RPM_BASE_DIR} --define '_product_version 0.1'
+		_logStage "######## rpmbuild finished! ... ########"
 	done
 
-# Move to Target
+_logStage "######## Moving to target folder... ########"
 find "${RPM_BASE_DIR}/RPMS" -type f -name '*.rpm' -exec cp {} "${BASE_DIR}/target" \;
 fi
+
+_logStage "######## RPM Stage Finished! ... ########"
