eade556e22bb97c7118b68e2b4ef47a7ff04db90
xval <xval@hi-iberia.es>
2014-08-12 10:46:43 +0200

    updated readme and parameters info

--- resources/mysql-scripts/mysql-columnsync.sh
+++ resources/mysql-scripts/mysql-columnsync.sh
@@ -3,49 +3,52 @@
 #
 # This file is part of fiware-connectors.
 #
-# Orion Context Broker is free software: you can redistribute it and/or
+# Fiware-connectors is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# Orion Context Broker is distributed in the hope that it will be useful,
+# Fiware-connectors is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 # General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+# along with fiware-connectors. If not, see http://www.gnu.org/licenses/.
 #
 # For those usages not covered by this license please contact with
-# xval@hi-iberia.es
+# francisco.romerobueno@telefonica.com
+# Author xval
 
 # This script is aimed to Cygnus debugging. It creates the needed database and table in MySQL 
 # when the sync between CYGNUS and MySQL is set to COLUMN
-# It uses six arguments: 
-# MYSQL_HOST
-# MYSQL_USER It has to be allowed to create dbs and tables
-# MYSQL_PASS User pass
-# ENTITY_SERVICE Service Name like FIWARE-Service in Orion headers
-# ENTITY_TYPE Type of entity (for this example is type "room")
-# ENTITY_ID ID or name of the entity that is sync (for this example is type "room99")
-# (Optional) CYGNUS_PREFIX 
+# It uses seven arguments (plus and additional one):
+# 1 MYSQL_HOST
+# 2 MYSQL_USER It has to be allowed to create dbs and tables
+# 3 MYSQL_PASS User pass
+# 4 ENTITY_SERVICE Service Name like FIWARE-Service in Orion headers
+# 5 ENTITY_TYPE Type of entity (for this example is type "room")
+# 6 ENTITY_ID ID or name of the entity that is sync (for this example is type "room99")
+# 7 ENTITY_ATT or the attribute type of value to store (for this example is used "temperature")
+# 8 (Optional) CYGNUS_PREFIX 
 
 # USAGES:
 # WITHOUT PREFIX 
-# $ ./create-dbsinkplace.sh ipmysql dbuser dbpass service1 room room99
+# $ ./create-dbsinkplace.sh ipmysql dbuser dbpass service1 room room99 temperature
 # USING PREFIX 
-# $ ./create-dbsinkplace.sh ipmysql dbuser dbpass service2 room room99 pre_
+# $ ./create-dbsinkplace.sh ipmysql dbuser dbpass service2 room room99 temperature pre_
 
-# It should create a table to store TIMESTAMP TEMPERATURE and TEMPERATURE_METADATA if you need order fields, just customize it
+# It should create a table to store TIMESTAMP, ENTITY_ATT and ENTITY_ATT_METADATA if you need extra fields, just customize it.
 MYSQL_HOST=$1
 MYSQL_USER=$2
 MYSQL_PASS=$3
 ENTITY_SERVICE=$4
 ENTITY_TYPE=$5
 ENTITY_ID=$6
+ENTITY_ATT=$7
 
-if [[ $7 -eq 0 ]] ; then
-    CYGNUS_PREFIX=$7
+if [[ $8 -eq 0 ]] ; then
+    CYGNUS_PREFIX=$8
 	MYSQL_DBNAME=${CYGNUS_PREFIX}${ENTITY_SERVICE}
 	MYSQL_TABLE=${CYGNUS_PREFIX}${ENTITY_ID}_${ENTITY_TYPE}
 else
@@ -60,8 +63,8 @@ echo "DROP DATABASE IF EXISTS ${MYSQL_DBNAME};
 	DROP TABLE IF EXISTS ${MYSQL_TABLE};
 	CREATE TABLE IF NOT EXISTS ${MYSQL_DBNAME} (
 	recvTime timestamp NULL DEFAULT NULL,
-	temperature varchar(50) DEFAULT NULL,
-	temperature_md varchar(50) DEFAULT NULL
+	${ENTITY_ATT} varchar(50) DEFAULT NULL,
+	${ENTITY_ATT}_md varchar(50) DEFAULT NULL
 	);" | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS}
 
 echo "TASK COMPLETED!!
