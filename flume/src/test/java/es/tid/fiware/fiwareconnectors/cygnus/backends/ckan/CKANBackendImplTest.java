cef1a376d9ffcd26e189295dc671fa6e5bdf66d9
frbattid <frb@tid.es>
2014-09-22 18:18:17 +0200

    Add unit tests for all the backends, the Jetty server and the management interface

---
+++ flume/src/test/java/es/tid/fiware/fiwareconnectors/cygnus/backends/ckan/CKANBackendImplTest.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-connectors (FI-WARE project).
+ *
+ * fiware-connectors is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * fiware-connectors is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with fiware-connectors. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please contact with Francisco Romero
+ * francisco.romerobueno@telefonica.com
+ */
+
+package es.tid.fiware.fiwareconnectors.cygnus.backends.ckan;
+
+import es.tid.fiware.fiwareconnectors.cygnus.backends.mysql.MySQLBackend;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ *
+ * @author frb
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CKANBackendImplTest {
+    
+    // instance to be tested
+    private CKANBackendImpl backend;
+    
+    // constants
+    private final String host = "localhost";
+    private final String port = "3306";
+    private final String user = "root";
+    private final String password = "12345";
+    
+    /**
+     * Sets up tests by creating a unique instance of the tested class, and by defining the behaviour of the mocked
+     * classes.
+     *  
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        // set up the instance of the tested class
+    } // setUp
+    
+    /**
+     * Test of xxxxx method, of class MySQLBackend.
+     */
+    @Test
+    public void testCreateDatabase() {
+        System.out.println("Testing MySQLBackend.createDatabase");
+    } // testCreateDatabase
+    
+    
+} // CKANBackendImplTest
