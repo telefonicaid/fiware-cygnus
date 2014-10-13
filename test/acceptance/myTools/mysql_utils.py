6d6d63d7b9dc224ff46324c8192794db8f0b267a
Ivan Arias Leon <ivanl@tid.es>
2014-10-13 15:25:51 +0200

    [FIX] several comments

--- test/acceptance/myTools/mysql_utils.py
+++ test/acceptance/myTools/mysql_utils.py
@@ -4,10 +4,10 @@
 #
 # This file is part of fiware-connectors (FI-WARE project).
 #
-# cosmos-injector is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# fiware-connectors is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
 # Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
-# cosmos-injector is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# fiware-connectors is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
 #
@@ -15,7 +15,7 @@
 # http://www.gnu.org/licenses/.
 #
 # For those usages not covered by the GNU Affero General Public License please contact with Francisco Romero
-# frb@tid.es
+# francisco.romerobueno@telefonica.com
 #
 #     Author: Ivan Arias
 #
