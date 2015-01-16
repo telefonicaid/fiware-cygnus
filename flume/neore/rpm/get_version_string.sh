97f56e68bb78d3b4711b8e95edc56f5d42c9494a
Alvaro Olmedo <alvaro.olmedo@tic.alten.es>
2015-01-16 09:19:35 +0100

    Changing header

--- flume/neore/rpm/get_version_string.sh
+++ flume/neore/rpm/get_version_string.sh
@@ -2,20 +2,20 @@
 #
 # Copyright 2013 Telefonica Investigacion y Desarrollo, S.A.U
 #
-# This file is part of Orion Context Broker.
+# This file is part of fiware-connectors (FI-WARE project).
 #
-# Orion Context Broker is free software: you can redistribute it and/or
+# Fiware-connectors is free software: you can redistribute it and/or
 # modify it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# Orion Context Broker is distributed in the hope that it will be useful,
+# Fiware-connectors is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
 # General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+# along with Fiware-connectors. If not, see http://www.gnu.org/licenses/.
 #
 # For those usages not covered by this license please contact with
 # iot_support at tid dot es
