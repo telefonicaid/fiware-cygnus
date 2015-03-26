4cddfeda610eab6558ca1b99363c166fdbee4699
frb <frb@MAC-510380.local>
2015-03-26 11:50:56 +0100

    MODIFY the base name of the packages, new is com.telefonica.com.cygnus

---
+++ src/main/java/com/telefonica/iot/cygnus/backends/ckan/CKANBackend.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-connectors (FI-WARE project).
+ *
+ * fiware-connectors is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * fiware-connectors is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with fiware-connectors. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please contact with iot_support at tid dot es
+ */
+
+package com.telefonica.iot.cygnus.backends.ckan;
+
+import java.util.Map;
+
+/**
+ * Interface for those backends implementing the persistence in CKAN.
+ * 
+ * @author fermin
+ */
+public interface CKANBackend {
+
+    /**
+     * Persist data in the CKAN datastore associated with the entity in a given organization (row mode).
+     * 
+     * @param recvTimeTs Reception time in milliseconds
+     * @param recvTime Reception time (human readable)
+     * @param orgName Organization name
+     * @param pkgName Package/dataset name
+     * @param resName Resource name
+     * @param attrName Attribute name
+     * @param attrType Attribute type
+     * @param attrValue Attribute value
+     * @param attrMd Attribute metadata string serialization
+     * @throws Exception
+     */
+    void persist(long recvTimeTs, String recvTime, String orgName, String pkgName, String resName, String attrName,
+            String attrType, String attrValue, String attrMd) throws Exception;
+
+    /**
+     * Persist data in the CKAN datastore associated with the entity in a given organization (column mode).
+     *
+     * @param recvTime Reception time (human readable)
+     * @param orgName Organization name
+     * @param pkgName Package/dataset name
+     * @param resName Resource name
+     * @param attrList Hashmap containing the attributes to persist
+     * @param attrMdList Hashmap containing the metadata string serialization to persist
+     * @throws Exception
+     */
+    void persist(String recvTime, String orgName, String pkgName, String resName, Map<String, String> attrList,
+            Map<String, String> attrMdList) throws Exception;
+    
+} // CKANBackend
---
+++ src/main/java/com/telefonica/iot/cygnus/sinks/OrionTestSink.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-connectors (FI-WARE project).
+ *
+ * fiware-connectors is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. fiware-connectors is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with fiware-connectors. If not, see http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please
+ * contact with iot_support at tid dot es
+ */
+package com.telefonica.iot.cygnus.sinks;
+
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest.ContextAttribute;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest.ContextElement;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest.ContextElementResponse;
+import com.telefonica.iot.cygnus.log.CygnusLogger;
+import com.telefonica.iot.cygnus.utils.Constants;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.apache.flume.Context;
+
+/**
+ * Sink for testing purposes. It does not persist the notified context data but
+ * prints logs about it. This can configured by the users in order to test the
+ * connectivity with Orion Context Broker.
+ *
+ * @author frb
+ */
+public class OrionTestSink extends OrionSink {
+
+    private static final CygnusLogger LOGGER = new CygnusLogger(OrionTestSink.class);
+
+    /**
+     * Constructor.
+     */
+    public OrionTestSink() {
+        super();
+        //cygnusLogger = new CygnusLogger(LoggerFactory.getLogger(OrionTestSink.class), true);
+    } // OrionTestSink
+
+    @Override
+    public void configure(Context context) {
+        // nothing to configure... this is a testing sink and should be simple!
+    } // configure
+
+    @Override
+    public void start() {
+        super.start();
+        LOGGER.info("[" + this.getName() + "] Startup completed");
+    } // start
+
+    @Override
+    void persist(Map<String, String> eventHeaders, NotifyContextRequest notification) throws Exception {
+        // get some header values
+        Long recvTimeTs = new Long(eventHeaders.get("timestamp"));
+        String fiwareService = eventHeaders.get(Constants.HEADER_SERVICE);
+        String fiwareServicePath = eventHeaders.get(Constants.HEADER_SERVICE_PATH);
+        String[] destinations = eventHeaders.get(Constants.DESTINATION).split(",");
+
+        // human readable version of the reception time
+        String recvTime = new Timestamp(recvTimeTs).toString().replaceAll(" ", "T");
+        
+        // lob about the event headers with deliberated INFO level
+        LOGGER.info("[" + this.getName() + "] Processing headers (recvTimeTs=" + recvTimeTs + " (" + recvTime
+                + "), fiwareService=" + fiwareService + ", fiwareServicePath=" + fiwareServicePath
+                + ", destinations=" + Arrays.toString(destinations) + ")");
+        
+        // iterate on the contextResponses
+        ArrayList<ContextElementResponse> contextResponses = notification.getContextResponses();
+
+        for (ContextElementResponse contextElementResponse: contextResponses) {
+            ContextElement contextElement = contextElementResponse.getContextElement();
+            String entityId = contextElement.getId();
+            String entityType = contextElement.getType();
+            
+            // log about the context element with deliberated INFO level
+            LOGGER.info("[" + this.getName() + "] Processing context element (id=" + entityId + ", type= "
+                    + entityType + ")");
+
+            // iterate on all this entity's attributes, if there are attributes
+            ArrayList<ContextAttribute> contextAttributes = contextElement.getAttributes();
+
+            if (contextAttributes == null || contextAttributes.isEmpty()) {
+                LOGGER.warn("No attributes within the notified entity, nothing is done (id=" + entityId
+                        + ", type=" + entityType + ")");
+                continue;
+            } // if
+
+            for (ContextAttribute contextAttribute : contextAttributes) {
+                String attrName = contextAttribute.getName();
+                String attrType = contextAttribute.getType();
+                String attrValue = contextAttribute.getContextValue(false);
+                String attrMetadata = contextAttribute.getContextMetadata();
+                
+                // log about the context attribute with deliberated INFO level
+                LOGGER.info("[" + this.getName() + "] Processing context attribute (name=" + attrName + ", type="
+                        + attrType + ", value=" + attrValue + ", metadata=" + attrMetadata + ")");
+            } // for
+        } // for
+    } // persist
+
+} // OrionTestSink
