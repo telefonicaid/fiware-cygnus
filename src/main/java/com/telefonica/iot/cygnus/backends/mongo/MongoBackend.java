ff6ad7eaf8357303f9cfff4ba011e2cf6a8ce72b
frb <frb@MAC-510380.local>
2015-04-21 13:17:28 +0200

    ADD a backend for Mongo

---
+++ src/main/java/com/telefonica/iot/cygnus/backends/mongo/MongoBackend.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-cygnus (FI-WARE project).
+ *
+ * fiware-cygnus is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * fiware-cygnus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with fiware-cygnus. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please contact with iot_support at tid dot es
+ */
+package com.telefonica.iot.cygnus.backends.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.telefonica.iot.cygnus.backends.mysql.MySQLBackend;
+import com.telefonica.iot.cygnus.log.CygnusLogger;
+import com.telefonica.iot.cygnus.sinks.OrionMongoSink.DataModel;
+import java.util.Arrays;
+import java.util.Map;
+import org.bson.Document;
+
+/**
+ *
+ * @author frb
+ */
+public class MongoBackend {
+    
+    private final String mongoHost;
+    private final int mongoPort;
+    private final String mongoUsername;
+    private final String mongoPassword;
+    private final DataModel dataModel;
+    private static final CygnusLogger LOGGER = new CygnusLogger(MySQLBackend.class);
+            
+    /**
+     * Constructor.
+     * @param mongoHost
+     * @param mongoPort
+     * @param mongoUsername
+     * @param mongoPassword
+     * @param dataModel
+     */
+    public MongoBackend(String mongoHost, int mongoPort, String mongoUsername, String mongoPassword,
+            DataModel dataModel) {
+        this.mongoHost = mongoHost;
+        this.mongoPort = mongoPort;
+        this.mongoUsername = mongoUsername;
+        this.mongoPassword = mongoPassword;
+        this.dataModel = dataModel;
+    } // MongoBackend
+    
+    /**
+     * Creates a database, given its name, if not exists.
+     * @param dbName
+     * @throws Exception
+     */
+    public void createDatabase(String dbName) throws Exception {
+        LOGGER.debug("Creating Mongo database=" + dbName);
+        getDatabase(dbName); // getting a non existent database automatically creates it
+    } // createDatabase
+    
+    /**
+     * Creates a collection, given its name, if not exists in the given database.
+     * @param dbName
+     * @param collectionName
+     * @throws Exception
+     */
+    public void createCollection(String dbName, String collectionName) throws Exception {
+        LOGGER.debug("Creating Mongo collection=" + collectionName + " at database=" + dbName);
+        MongoDatabase db = getDatabase(dbName);
+
+        if (db.getCollection(collectionName) == null) {
+            db.createCollection(collectionName);
+        } // if
+    } // createCollection
+    
+    /**
+     * Inserts a new document in the given raw collection within the given database (row-like mode).
+     * @param dbName
+     * @param collectionName
+     * @param recvTimeTs
+     * @param recvTime
+     * @param entityId
+     * @param entityType
+     * @param attrName
+     * @param attrType
+     * @param attrValue
+     * @param attrMd
+     * @throws Exception
+     */
+    public void insertContextDataRaw(String dbName, String collectionName, long recvTimeTs, String recvTime,
+            String entityId, String entityType, String attrName, String attrType, String attrValue,
+            String attrMd) throws Exception {
+        MongoDatabase db = getDatabase(dbName);
+        MongoCollection collection = db.getCollection(collectionName);
+        Document doc = new Document("recvTime", recvTime);
+        
+        switch (dataModel) {
+            case COLLECTIONPERSERVICEPATH:
+                doc.append("entityId", entityId)
+                        .append("entityType", entityType)
+                        .append("attrName", attrName)
+                        .append("attrType", attrType)
+                        .append("attrValue", attrValue);
+                break;
+            case COLLECTIONPERENTITY:
+                doc.append("attrName", attrName)
+                        .append("attrType", attrType)
+                        .append("attrValue", attrValue);
+                break;
+            case COLLECTIONPERATTRIBUTE:
+                doc.append("attrType", attrType)
+                        .append("attrValue", attrValue);
+                break;
+            default:
+                // this will never be reached
+        } // switch
+
+        LOGGER.debug("Inserting data=" + doc.toString() + " within collection=" + collectionName);
+        collection.insertOne(doc);
+    } // insertContextDataRaw
+    
+    /**
+     * Inserts a new document in the given raw collection within the given database (column-like mode).
+     * changes.
+     * @param dbName
+     * @param collectionName
+     * @param recvTime
+     * @param attrs
+     * @param mds
+     * @throws Exception
+     */
+    public void insertContextDataRaw(String dbName, String collectionName, String recvTime,
+            Map<String, String> attrs, Map<String, String> mds) throws Exception {
+        // FIXME: the row-like column insertion mode is not currently available for Mongo
+    } // insertContextDataRaw
+    
+    private MongoDatabase getDatabase(String dbName) {
+        ServerAddress server = new ServerAddress(mongoHost, mongoPort);
+        MongoClient client;
+        
+        if (mongoUsername.length() != 0) {
+            MongoCredential credential = MongoCredential.createCredential(mongoUsername, dbName,
+                    mongoPassword.toCharArray());
+            client = new MongoClient(server, Arrays.asList(credential));
+        } else {
+            client = new MongoClient(server);
+        } // if else
+        
+        return client.getDatabase(dbName);
+    } // getDatabase
+    
+} // MongoBackend
