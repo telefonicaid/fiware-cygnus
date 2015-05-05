f16e0eb57db0c58d5be3e07c7a6182198cf2a544
frb <frb@MAC-510380.local>
2015-05-05 08:28:48 +0200

    MODIFY OrionMongoSink by extending OrionMongoBaseSink

--- src/main/java/com/telefonica/iot/cygnus/sinks/OrionMongoSink.java
+++ src/main/java/com/telefonica/iot/cygnus/sinks/OrionMongoSink.java
@@ -17,16 +17,11 @@
  */
 package com.telefonica.iot.cygnus.sinks;
 
-import com.telefonica.iot.cygnus.backends.mongo.MongoBackend;
 import com.telefonica.iot.cygnus.containers.NotifyContextRequest;
-import com.telefonica.iot.cygnus.errors.CygnusBadConfiguration;
-import com.telefonica.iot.cygnus.log.CygnusLogger;
 import com.telefonica.iot.cygnus.utils.Constants;
 import com.telefonica.iot.cygnus.utils.Utils;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
-import org.apache.flume.Context;
 
 /**
  * OrionMongoSink will be in charge of persisting Orion context data in a historic fashion within a MongoDB deployment.
@@ -42,21 +37,7 @@ import org.apache.flume.Context;
  * 
  * @author frb
  */
-public class OrionMongoSink extends OrionSink {
-    
-    /**
-     * Available data models when persisting data at Mongo.
-     */
-    public enum DataModel { COLLECTIONPERSERVICEPATH, COLLECTIONPERENTITY, COLLECTIONPERATTRIBUTE }
-    
-    private static final CygnusLogger LOGGER = new CygnusLogger(OrionMongoSink.class);
-    private String mongoHosts;
-    private String mongoUsername;
-    private String mongoPassword;
-    private DataModel dataModel;
-    private String dbPrefix;
-    private String collectionPrefix;
-    private MongoBackend backend;
+public class OrionMongoSink extends OrionMongoBaseSink {
 
     /**
      * Constructor.
@@ -64,103 +45,7 @@ public class OrionMongoSink extends OrionSink {
     public OrionMongoSink() {
         super();
     } // OrionMongoSink
-    
-    /**
-     * Gets the mongo URI. It is protected since it is used by the tests.
-     * @return
-     */
-    protected String getURI() {
-        return mongoHosts;
-    } // getURI
-    
-    /**
-     * Gets the mongo username. It is protected since it is used by the tests.
-     * @return
-     */
-    protected String getUsername() {
-        return mongoUsername;
-    } // getUsername
-    
-    /**
-     * Gets the mongo password. It is protected since it is used by the tests.
-     * @return
-     */
-    protected String getPassword() {
-        return mongoPassword;
-    } // getPassword
-    
-    /**
-     * Gets the mongo data model. It is protected since it is used by the tests.
-     * @return
-     */
-    protected DataModel getDataModel() {
-        return dataModel;
-    } // getDataModel
-    
-    /**
-     * Gets the database prefix. It is protected since it is used by the tests.
-     * @return
-     */
-    protected String getDbPrefix() {
-        return dbPrefix;
-    } // getDBPrefix
-    
-    /**
-     * Gets the collection prefix. It is protected since it is used by the tests.
-     * @return
-     */
-    protected String getCollectionPrefix() {
-        return collectionPrefix;
-    } // getCollectionPrefix
-    
-    /**
-     * Sets the backend. It is protected since it is used by the tests.
-     * @param backend
-     */
-    protected void setBackend(MongoBackend backend) {
-        this.backend = backend;
-    } // setBackend
-    
-    /**
-     * Gets the backend. It is protected since it is used by the tests.
-     * @return
-     */
-    protected MongoBackend getBackend() {
-        return backend;
-    } // getBackend
-    
-    @Override
-    public void configure(Context context) {
-        mongoHosts = context.getString("mongo_hosts", "localhost:27017");
-        LOGGER.debug("[" + this.getName() + "] Reading configuration (mongo_hosts=" + mongoHosts + ")");
-        mongoUsername = context.getString("mongo_username", "");
-        LOGGER.debug("[" + this.getName() + "] Reading configuration (mongo_username=" + mongoUsername + ")");
-        // FIXME: mongoPassword should be read as a SHA1 and decoded here
-        mongoPassword = context.getString("mongo_password", "");
-        LOGGER.debug("[" + this.getName() + "] Reading configuration (mongo_password=" + mongoPassword + ")");
-        dataModel = getDataModel(context.getString("data_model", "collection-per-attribute"));
-        
-        if (dataModel == null) {
-            LOGGER.error("Invalid data model, using 'collection-per-attribute' by default");
-            dataModel = DataModel.COLLECTIONPERATTRIBUTE;
-        } // if
-        
-        LOGGER.debug("[" + this.getName() + "] Reading configuration (data_model=" + dataModel + ")");
-        dbPrefix = Utils.encode(context.getString("db_prefix", "sth_"));
-        LOGGER.debug("[" + this.getName() + "] Reading configuration (db_prefix=" + dbPrefix + ")");
-        collectionPrefix = Utils.encode(context.getString("collection_prefix", "sth_"));
-        LOGGER.debug("[" + this.getName() + "] Reading configuration (collection_prefix=" + collectionPrefix + ")");
-    } // configure
-    
-    @Override
-    public void start() {
-        // create the persistence backend
-        backend = new MongoBackend(mongoHosts, mongoUsername, mongoPassword, dataModel);
-        LOGGER.debug("[" + this.getName() + "] Mongo persistence backend created");
-        super.start();
-        LOGGER.info("[" + this.getName() + "] Startup completed");
-    } // start
-    
+
     @Override
     void persist(Map<String, String> eventHeaders, NotifyContextRequest notification) throws Exception {
         // get some header values
@@ -212,16 +97,6 @@ public class OrionMongoSink extends OrionSink {
                         + ", type=" + entityType + ")");
                 continue;
             } // if
-            
-            // this is used for storing the attribute's names and values when dealing with a per column attributes
-            // persistence; in that case the persistence is not done attribute per attribute, but persisting all of them
-            // at the same time
-            HashMap<String, String> attrs = new HashMap<String, String>();
-            
-            // this is used for storing the attribute's names (sufixed with "-md") and metadata when dealing with a per
-            // column attributes persistence; in that case the persistence is not done attribute per attribute, but
-            // persisting all of them at the same time
-            HashMap<String, String> mds = new HashMap<String, String>();
 
             for (NotifyContextRequest.ContextAttribute contextAttribute : contextAttributes) {
                 String attrName = contextAttribute.getName();
@@ -246,72 +121,5 @@ public class OrionMongoSink extends OrionSink {
             } // for
         } // for
     } // persist
- 
-    /**
-     * Gets the data model given its string representation.
-     * @param dataModelStr
-     * @return
-     */
-    protected DataModel getDataModel(String dataModelStr) {
-        if (dataModelStr.equals("collection-per-service-path")) {
-            return DataModel.COLLECTIONPERSERVICEPATH;
-        } else if (dataModelStr.equals("collection-per-entity")) {
-            return DataModel.COLLECTIONPERENTITY;
-        } else if (dataModelStr.equals("collection-per-attribute")) {
-            return DataModel.COLLECTIONPERATTRIBUTE;
-        } else {
-            return null;
-        } // if else if
-    } // getDataModel
-
-    /**
-     * Builds a database name given a fiwareService. It throws an exception if the naming conventions are violated.
-     * @param fiwareService
-     * @return
-     * @throws Exception
-     */
-    private String buildDbName(String fiwareService) throws Exception {
-        String dbName = dbPrefix + fiwareService;
-        
-        if (dbName.length() > Constants.MAX_NAME_LEN) {
-            throw new CygnusBadConfiguration("Building dbName=fiwareService (" + dbName + ") and its length is greater "
-                    + "than " + Constants.MAX_NAME_LEN);
-        } // if
-        
-        return dbName;
-    } // buildDbName
-    
-    /**
-     * Builds a collection name given a fiwareServicePath and a destination. It throws an exception if the naming
-     * conventions are violated.
-     * @param fiwareServicePath
-     * @param destination
-     * @return
-     * @throws Exception
-     */
-    private String buildCollectionName(String fiwareServicePath, String destination, String attrName) throws Exception {
-        String collectionName = collectionPrefix;
-        
-        switch (dataModel) {
-            case COLLECTIONPERSERVICEPATH:
-                collectionName += fiwareServicePath;
-                break;
-            case COLLECTIONPERENTITY:
-                collectionName += fiwareServicePath + "_" + destination;
-                break;
-            case COLLECTIONPERATTRIBUTE:
-                collectionName += fiwareServicePath + "_" + destination + "_" + attrName;
-                break;
-            default:
-                // this will never be reached
-        } // switch
-
-        if (collectionName.length() > Constants.MAX_NAME_LEN) {
-            throw new CygnusBadConfiguration("Building collectionName=fiwareServicePath + '_' + destination ("
-                    + collectionName + ") and its length is greater than " + Constants.MAX_NAME_LEN);
-        } // if
-        
-        return collectionName;
-    } // buildCollectionName
 
 } // OrionMongoSink
