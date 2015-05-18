efb9a027c8da71e90edd2b69e7cb12cbc7ae9001
frb <frb@MAC-510380.local>
2015-05-18 14:27:58 +0200

    UPDATE the pattern-based grouping mechanism

--- src/main/java/com/telefonica/iot/cygnus/interceptors/GroupingInterceptor.java
+++ src/main/java/com/telefonica/iot/cygnus/interceptors/GroupingInterceptor.java
@@ -32,7 +32,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -43,6 +43,10 @@ import javax.xml.parsers.SAXParserFactory;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.interceptor.Interceptor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -52,46 +56,83 @@ import org.xml.sax.SAXException;
  * 
  * @author frb
  */
-public class DestinationExtractor implements Interceptor {
+public class GroupingInterceptor implements Interceptor {
     
-    private static final CygnusLogger LOGGER = new CygnusLogger(DestinationExtractor.class);
-    private final String matchingTableFile;
-    private ArrayList<MatchingRule> matchingTable;
+    private static final CygnusLogger LOGGER = new CygnusLogger(GroupingInterceptor.class);
+    private final String groupingRulesFileName;
+    private LinkedList<GroupingRule> groupingRules;
     
     /**
      * Constructor.
-     * @param matchingTableFile
+     * @param groupingRulesFileName
      */
-    public DestinationExtractor(String matchingTableFile) {
-        this.matchingTableFile = matchingTableFile;
-    } // DestinationExtractor
+    public GroupingInterceptor(String groupingRulesFileName) {
+        this.groupingRulesFileName = groupingRulesFileName;
+    } // GroupingInterceptor
     
     /**
-     * Gets the matching table. This is protected since it is only going to be used in the tests.
+     * Gets the grouping rules. This is protected since it is only going to be used in the tests.
      * @return
      */
-    protected ArrayList<MatchingRule> getMatchingTable() {
-        return matchingTable;
-    } // getMatchingTable
+    protected LinkedList<GroupingRule> getGroupingRules() {
+        return groupingRules;
+    } // getGroupingRules
     
     @Override
     public void initialize() {
-        // load the matching table from the file where it is described
-        matchingTable = new ArrayList<MatchingRule>();
+        // read the grouping rules file; a JSONParse(Reader) method cannot be used since the file may contain comment
+        // lines starting by the '#' character
+        String jsonStr = readGroupingRulesFile(groupingRulesFileName);
         
-        if (matchingTableFile == null) {
-            // despite configuring the interceptor, no table_matching.conf file has been specified
+        if (jsonStr == null) {
+            LOGGER.debug("No grouping rules read");
             return;
+        } // if
+        
+        LOGGER.debug("Grouping rules read: " + jsonStr);
+        
+        // parse the Json containing the grouping rules
+        JSONArray jsonGroupingRules = (JSONArray) parseGroupingRules(jsonStr);
+        
+        if (jsonGroupingRules == null) {
+            LOGGER.debug("Grouping rules syntax has errors");
+            return;
+        } // if
+        
+        LOGGER.debug("Grouping rules syntax is OK");
+        
+        // create a list of grouping rules, with precompiled regex
+        groupingRules = createGroupingRules(jsonGroupingRules);
+        
+        if (groupingRules == null) {
+            LOGGER.debug("Grouping rules regex'es could not be compiled");
+        }
+        
+        LOGGER.debug("Grouping rules regex'es have been compiled");
+    } // initialize
+    
+    /**
+     * Reads a file containing Json-based grouing rules. The file may contain lines representing comments, which start
+     * by the '#' character.
+     * @param fileName File to be read
+     * @return A string containing the Json, discarding the comment lines
+     */
+    private String readGroupingRulesFile(String fileName) {
+        String jsonStr = "";
+        
+        if (fileName == null) {
+            // despite configuring the interceptor, no table_matching.conf file has been specified
+            return null;
         } // if
         
         BufferedReader reader;
         
         try {
-            reader = new BufferedReader(new FileReader(matchingTableFile));
+            reader = new BufferedReader(new FileReader(groupingRulesFileName));
         } catch (FileNotFoundException e) {
-            LOGGER.error("Runtime error (File not found. Details=" + e.getMessage() + ")");
-            return;
-        } // try catch // try catch
+            LOGGER.error("File not found. Details=" + e.getMessage() + ")");
+            return null;
+        } // try catch
         
         String line;
         
@@ -101,22 +142,55 @@ public class DestinationExtractor implements Interceptor {
                     continue;
                 } // if
                 
-                String[] tokens = line.split("\\|");
-                
-                if (tokens.length < 5 || tokens[0].length() == 0 || tokens[1].length() == 0 || tokens[2].length() == 0
-                        || tokens[3].length() == 0 || tokens[4].length() == 0) {
-                    LOGGER.warn("Malformed matching rule, it will be discarded. Details=" + line);
-                    continue;
-                } // if
-                
-                int id = new Integer(tokens[0]);
-                ArrayList<String> fields = new ArrayList<String>(Arrays.asList(tokens[1].split(",")));
-                matchingTable.add(new MatchingRule(id, fields, tokens[2], tokens[3], tokens[4]));
+                jsonStr += line;
             } // while
+            
+            return jsonStr;
         } catch (IOException e) {
-            LOGGER.error("Runtime error (I/O exception. Details=" + e.getMessage() + ")");
-        } // try catch // try catch
-    } // initialize
+            LOGGER.error("Error while reading the Json-based grouping rules file. Details=" + e.getMessage() + ")");
+            return null;
+        } // try catch
+    } // readGroupingRulesFile
+    
+    private JSONArray parseGroupingRules(String jsonStr) {
+        if (jsonStr == null) {
+            return null;
+        } // if
+        
+        JSONParser jsonParser = new JSONParser();
+        
+        try {
+            return (JSONArray) ((JSONObject) jsonParser.parse(jsonStr)).get("grouping_rules");
+        } catch (ParseException e) {
+            LOGGER.error("Error while parsing the Json-based grouping rules file. Details=" + e.getMessage());
+            return null;
+        } // try catch
+    } // parseGroupingRules
+    
+    private LinkedList<GroupingRule> createGroupingRules(JSONArray jsonGroupingRules) {
+        if (jsonGroupingRules == null) {
+            return null;
+        } // if
+        
+        groupingRules = new LinkedList<GroupingRule>();
+        
+        for (Object jsonGroupingRule : jsonGroupingRules) {
+            JSONObject jsonRule = (JSONObject) jsonGroupingRule;
+            
+            if (isValid(jsonRule)) {
+                GroupingRule rule = new GroupingRule(jsonRule);
+                groupingRules.add(rule);
+            } // if
+        } // for
+        
+        return groupingRules;
+    } // createGroupingRules
+    
+    private boolean isValid(JSONObject jsonRule) {
+        return (jsonRule.containsKey("id") && jsonRule.containsKey("fields")
+                && jsonRule.containsKey("regex") && jsonRule.containsKey("destination")
+                && jsonRule.containsKey("fiware_service_path"));
+    } // isValid
  
     @Override
     public Event intercept(Event event) {
@@ -127,7 +201,8 @@ public class DestinationExtractor implements Interceptor {
         // get some original header values
         String fiwareServicePath = headers.get(Constants.HEADER_SERVICE_PATH);
         
-        // parse the original body
+        // parse the original body; this part may be unnecessary if notifications are parsed at the source only once
+        // see --> https://github.com/telefonicaid/fiware-cygnus/issues/359
         NotifyContextRequest notification;
 
         if (headers.get(Constants.HEADER_CONTENT_TYPE).contains("application/json")) {
@@ -166,7 +241,7 @@ public class DestinationExtractor implements Interceptor {
         
         // iterate on the contextResponses
         ArrayList<String> destinations = new ArrayList<String>();
-        ArrayList<String> datasets = new ArrayList<String>();
+        ArrayList<String> fiwareServicePaths = new ArrayList<String>();
         ArrayList<ContextElementResponse> contextResponses = notification.getContextResponses();
         
         if (contextResponses == null || contextResponses.isEmpty()) {
@@ -180,24 +255,25 @@ public class DestinationExtractor implements Interceptor {
             // iterate on the matching rules
             boolean added = false;
             
-            for (MatchingRule rule : matchingTable) {
-                String concat = concatenateFields(rule.fields, contextElement,
-                        headers.get(Constants.HEADER_SERVICE_PATH));
-                Matcher matcher = rule.pattern.matcher(concat);
-                
-                if (matcher.matches()) {
-                    destinations.add(rule.destination);
-                    datasets.add(rule.dataset);
-                    added = true;
-                    break;
-                } // if
-            } // for
+            if (groupingRules != null) {
+                for (GroupingRule rule : groupingRules) {
+                    String concat = concatenateFields(rule.getFields(), contextElement, fiwareServicePath);
+                    Matcher matcher = rule.pattern.matcher(concat);
+
+                    if (matcher.matches()) {
+                        destinations.add((String) rule.getDestination());
+                        fiwareServicePaths.add((String) rule.getNewFiwareServicePath());
+                        added = true;
+                        break;
+                    } // if
+                } // for
+            } // if
             
             // check if no matching was found, in that case the default destination ('<entityId>_<entityType>') and the
             // notified fiware-servicePath are used
             if (!added) {
                 destinations.add(Utils.encode(contextElement.getId() + "_" + contextElement.getType()));
-                datasets.add(fiwareServicePath);
+                fiwareServicePaths.add(fiwareServicePath);
             } // if
         } // for
  
@@ -205,7 +281,7 @@ public class DestinationExtractor implements Interceptor {
         headers.put(Constants.DESTINATION,
                 destinations.toString().replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", ""));
         headers.put(Constants.HEADER_SERVICE_PATH,
-                datasets.toString().replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", ""));
+                fiwareServicePaths.toString().replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", ""));
         event.setHeaders(headers);
         return event;
     } // intercept
@@ -230,16 +306,29 @@ public class DestinationExtractor implements Interceptor {
      * Builder class for this new interceptor.
      */
     public static class Builder implements Interceptor.Builder {
-        private String matchingTableFile;
+        private String groupingRulesFileName;
  
         @Override
         public void configure(Context context) {
-            matchingTableFile = context.getString("matching_table", null);
+            String groupingRulesFileNameTmp = context.getString("grouping_rules_conf_file");
+            String matchingTableFileName = context.getString("matching_table");
+            
+            if (groupingRulesFileNameTmp != null && groupingRulesFileNameTmp.length() > 0) {
+                groupingRulesFileName = groupingRulesFileNameTmp;
+                LOGGER.debug("[de] Reading configuration (grouping_rules_file=" + groupingRulesFileName + ")");
+            } else if (matchingTableFileName != null && matchingTableFileName.length() > 0) {
+                groupingRulesFileName = matchingTableFileName;
+                LOGGER.debug("[de] Reading configuration (matching_table=" + groupingRulesFileName + ")"
+                        + " -- DEPRECATED, use grouping_rules_file instead");
+            } else {
+                groupingRulesFileName = null;
+                LOGGER.debug("[de] Defaulting to grouping_rules_file=null");
+            } // if else
         } // configure
  
         @Override
         public Interceptor build() {
-            return new DestinationExtractor(matchingTableFile);
+            return new GroupingInterceptor(groupingRulesFileName);
         } // build
     } // Builder
     
@@ -262,35 +351,31 @@ public class DestinationExtractor implements Interceptor {
     /**
      * Each one of the entries of the matching table.
      */
-    protected class MatchingRule {
+    protected class GroupingRule {
         
-        private final int id;
-        private ArrayList<String> fields;
-        private Pattern pattern;
-        private String destination;
-        private String dataset;
+        private final long id;
+        private final ArrayList<String> fields;
+        private final Pattern pattern;
+        private final String destination;
+        private final String newFiwareServicePath;
         
         /**
          * Constructor.
-         * @param id
-         * @param fields
-         * @param regex
-         * @param destination
-         * @param group
+         * @param jsonRule
          */
-        public MatchingRule(int id, ArrayList<String> fields, String regex, String destination, String group) {
-            this.id = id;
-            this.fields = fields;
-            this.pattern = Pattern.compile(regex);
-            this.destination = Utils.encode(destination);
-            this.dataset = Utils.encode(group);
-        } // MatchingRule
+        public GroupingRule(JSONObject jsonRule) {
+            this.id = (Long) jsonRule.get("id");
+            this.fields = (JSONArray) jsonRule.get("fields");
+            this.pattern = Pattern.compile((String) jsonRule.get("regex"));
+            this.destination = Utils.encode((String) jsonRule.get("destination"));
+            this.newFiwareServicePath = Utils.encode((String) jsonRule.get("fiware_service_path"));
+        } // GroupingRule
         
         /**
          * Gets the rule's id.
          * @return
          */
-        public int getId() {
+        public long getId() {
             return id;
         } // getId
         
@@ -319,13 +404,13 @@ public class DestinationExtractor implements Interceptor {
         } // destination
         
         /**
-         * Gets the rule's dataset.
-         * @return The rule's dataset.
+         * Gets the rule's newFiwareServicePath.
+         * @return The rule's newFiwareServicePath.
          */
-        public String getDataset() {
-            return dataset;
-        } // getDataset
+        public String getNewFiwareServicePath() {
+            return newFiwareServicePath;
+        } // getNewFiwareServicePath
         
-    } // MatchingRule
+    } // GroupingRule
   
 } // NotificationSpliter
