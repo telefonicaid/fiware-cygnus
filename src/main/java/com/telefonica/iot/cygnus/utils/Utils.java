c3498b2bd4d90d804e4fa00e809d34693130c090
frb <frb@MAC-510380.hi.inet>
2015-06-01 09:38:14 +0200

    src/main/java/com/telefonica/iot/cygnus/utils/Utils.java

--- src/main/java/com/telefonica/iot/cygnus/utils/Utils.java
+++ src/main/java/com/telefonica/iot/cygnus/utils/Utils.java
@@ -165,16 +165,17 @@ public final class Utils {
     /**
      * Gets the human redable version of timestamp expressed in miliseconds.
      * @param ts
+     * @param addUTC
      * @return
      */
-    public static String getHumanReadable(long ts) {
+    public static String getHumanReadable(long ts, boolean addUTC) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         String humanRedable = sdf.format(new Date(ts));
         humanRedable += "T";
         sdf = new SimpleDateFormat("HH:mm:ss.S");
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        humanRedable += sdf.format(new Date(ts)) + "Z";
+        humanRedable += sdf.format(new Date(ts)) + (addUTC ? "Z" : "");
         return humanRedable;
     } // getHumanRedable
     
