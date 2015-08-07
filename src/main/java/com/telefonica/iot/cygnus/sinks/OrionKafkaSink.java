8e1eb6e6aec6982c4ecd399b0a92e0e4511709fa
frbattid <francisco.romerobueno@telefonica.com>
2015-08-07 09:38:53 +0200

    ADD different persistence modes, depending on the topic choosen

--- src/main/java/com/telefonica/iot/cygnus/sinks/OrionKafkaSink.java
+++ src/main/java/com/telefonica/iot/cygnus/sinks/OrionKafkaSink.java
@@ -18,13 +18,18 @@
 package com.telefonica.iot.cygnus.sinks;
 
 import com.telefonica.iot.cygnus.containers.NotifyContextRequest;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest.ContextAttribute;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest.ContextElement;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest.ContextElementResponse;
 import com.telefonica.iot.cygnus.log.CygnusLogger;
 import com.telefonica.iot.cygnus.utils.Constants;
 import com.telefonica.iot.cygnus.utils.Utils;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Properties;
 import org.apache.flume.Context;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 /**
  *
@@ -32,19 +37,35 @@ import org.apache.kafka.clients.producer.KafkaProducer;
  */
 public class OrionKafkaSink extends OrionSink {
     
+    /**
+     * Available topic types.
+     */
+    public enum TopicType { TOPICBYENTITYID, TOPICBYENTITYTYPE, TOPICBYSERVICEPATH, TOPICBYSERVICE }
+    
     private static final CygnusLogger LOGGER = new CygnusLogger(OrionKafkaSink.class);
     private KafkaProducer persistenceBackend;
+    private TopicType topicType;
+    private String brokerList;
     
     @Override
-    public void configure(Context cntxt) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void configure(Context context) {
+        String topicTypeStr = context.getString("topic_type", "topic-by-entity-id");
+        topicType = TopicType.valueOf(topicTypeStr.replaceAll("-", "").toUpperCase());
+        LOGGER.debug("[" + this.getName() + "] Reading configuration (topic_type=" + topicTypeStr + ")");
+        brokerList = context.getString("broker_list", "localhost:9092");
+        LOGGER.debug("[" + this.getName() + "] Reading configuration (broker_list=" + brokerList + ")");
     } // configure
     
     @Override
     public void start() {
         try {
             // create the persistence backend
-            persistenceBackend = new KafkaProducer();
+            Properties props = new Properties();
+            props.put("metadata.broker.list", brokerList);
+            props.put("serializer.class", "kafka.serializer.StringEncoder");
+            props.put("producer.type", "sync");
+            props.put("request.required.acks", "1");
+            persistenceBackend = new KafkaProducer(props);
             LOGGER.debug("[" + this.getName() + "] Kafka persistence backend (KafkaProducer) created");
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
@@ -55,28 +76,27 @@ public class OrionKafkaSink extends OrionSink {
     } // start
 
     @Override
-    void persist(Map<String, String> eventHeaders, NotifyContextRequest notification) throws Exception {
+    void persist(Map<String, String> eventHeaders, NotifyContextRequest notification) throws Exception {       
         // get some header values
         Long recvTimeTs = new Long(eventHeaders.get("timestamp"));
         String fiwareService = eventHeaders.get(Constants.HEADER_SERVICE);
         String[] fiwareServicePaths = eventHeaders.get(Constants.HEADER_SERVICE_PATH).split(",");
-        String[] destinations = eventHeaders.get(Constants.DESTINATION).split(",");
+//        String[] destinations = eventHeaders.get(Constants.DESTINATION).split(",");
         
         // human readable version of the reception time
         String recvTime = Utils.getHumanReadable(recvTimeTs, true);
-        
+
         // iterate on the contextResponses
         ArrayList contextResponses = notification.getContextResponses();
         
         for (int i = 0; i < contextResponses.size(); i++) {
             // get the i-th contextElement
-            NotifyContextRequest.ContextElementResponse contextElementResponse = (NotifyContextRequest.ContextElementResponse) contextResponses.get(i);
-            NotifyContextRequest.ContextElement contextElement = contextElementResponse.getContextElement();
+            ContextElementResponse contextElementResponse = (ContextElementResponse) contextResponses.get(i);
+            ContextElement contextElement = contextElementResponse.getContextElement();
             String entityId = contextElement.getId();
             String entityType = contextElement.getType();
             LOGGER.debug("[" + this.getName() + "] Processing context element (id=" + entityId + ", type="
                     + entityType + ")");
-            
             
             // iterate on all this entity's attributes, if there are attributes
             ArrayList<NotifyContextRequest.ContextAttribute> contextAttributes = contextElement.getAttributes();
@@ -87,74 +107,49 @@ public class OrionKafkaSink extends OrionSink {
                 continue;
             } // if
             
-            // this is used for storing the attribute's names and values in a Json-like way when dealing with a per
-            // column attributes persistence; in that case the persistence is not done attribute per attribute, but
-            // persisting all of them at the same time
             String columnLine = "{\"" + Constants.RECV_TIME + "\":\"" + recvTime + "\",";
-            
-            // this is used for storing the attribute's names needed by Hive in order to create the table when dealing
-            // with a per column attributes persistence; in that case the Hive table creation is not done using
-            // standard 8-fields but a variable number of them
-            String hiveFields = Constants.RECV_TIME + " string";
 
-            for (NotifyContextRequest.ContextAttribute contextAttribute : contextAttributes) {
+            for (ContextAttribute contextAttribute : contextAttributes) {
                 String attrName = contextAttribute.getName();
                 String attrType = contextAttribute.getType();
                 String attrValue = contextAttribute.getContextValue(true);
                 String attrMetadata = contextAttribute.getContextMetadata();
                 LOGGER.debug("[" + this.getName() + "] Processing context attribute (name=" + attrName + ", type="
                         + attrType + ")");
-                
-                if (rowAttrPersistence) {
-                    // create a Json document to be persisted
-                    String rowLine = "{"
-                            + "\"" + Constants.RECV_TIME_TS + "\":\"" + recvTimeTs / 1000 + "\","
-                            + "\"" + Constants.RECV_TIME + "\":\"" + recvTime + "\","
-                            + "\"" + Constants.ENTITY_ID + "\":\"" + entityId + "\","
-                            + "\"" + Constants.ENTITY_TYPE + "\":\"" + entityType + "\","
-                            + "\"" + Constants.ATTR_NAME + "\":\"" + attrName + "\","
-                            + "\"" + Constants.ATTR_TYPE + "\":\"" + attrType + "\","
-                            + "\"" + Constants.ATTR_VALUE + "\":" + attrValue + ","
-                            + "\"" + Constants.ATTR_MD + "\":" + attrMetadata
-                            + "}";
-                    LOGGER.info("[" + this.getName() + "] Persisting data at OrionHDFSSink. HDFS file ("
-                            + hdfsFile + "), Data (" + rowLine + ")");
-                    
-                    // if the fileName exists, append the Json document to it; otherwise, create it with initial content
-                    // and mark as existing (this avoids checking if the fileName exists each time a Json document is
-                    // going to be persisted)
-                    if (fileExists) {
-                        persistenceBackend.append(hdfsFile, rowLine);
-                    } else {
-                        persistenceBackend.createDir(hdfsFolder);
-                        persistenceBackend.createFile(hdfsFile, rowLine);
-                        persistenceBackend.provisionHiveTable(hdfsFolder);
-                        fileExists = true;
-                    } // if else
-                } else {
-                    columnLine += "\"" + attrName + "\":" + attrValue + ", \"" + attrName + "_md\":" + attrMetadata
-                            + ",";
-                    hiveFields += "," + attrName + " string," + attrName + "_md array<string>";
-                } // if else
+                columnLine += "\"" + attrName + "\":" + attrValue + ", \"" + attrName + "_md\":" + attrMetadata
+                        + ",";
             } // for
-                 
-            // if the attribute persistence mode is per column, now is the time to insert a new row containing full
-            // attribute list
-            if (!rowAttrPersistence) {
-                // insert a new row containing full attribute list
-                columnLine = columnLine.subSequence(0, columnLine.length() - 1) + "}";
-                LOGGER.info("[" + this.getName() + "] Persisting data at OrionHDFSSink. HDFS file (" + hdfsFile
-                        + "), Data (" + columnLine + ")");
-                
-                if (fileExists) {
-                    persistenceBackend.append(hdfsFile, columnLine);
-                } else {
-                    persistenceBackend.createDir(hdfsFolder);
-                    persistenceBackend.createFile(hdfsFile, columnLine);
-                    persistenceBackend.provisionHiveTable(hdfsFolder, hiveFields);
-                    fileExists = true;
-                } // if else
-            } // if
+            
+            // insert a new row containing full attribute list
+            columnLine = columnLine.subSequence(0, columnLine.length() - 1) + "}";
+            ProducerRecord record = null;
+ 
+            switch (topicType) {
+                case TOPICBYENTITYID:
+                    LOGGER.info("[" + this.getName() + "] Persisting data at OrionKafkaSink. Topic ("
+                            + entityId + "), Data (" + columnLine + ")");
+                    record = new ProducerRecord(entityId, columnLine);
+                    break;
+                case TOPICBYENTITYTYPE:
+                    LOGGER.info("[" + this.getName() + "] Persisting data at OrionKafkaSink. Topic ("
+                            + entityType + "), Data (" + columnLine + ")");
+                    record = new ProducerRecord(entityType, columnLine);
+                    break;
+                case TOPICBYSERVICEPATH:
+                    LOGGER.info("[" + this.getName() + "] Persisting data at OrionKafkaSink. Topic ("
+                            + fiwareServicePaths[i] + "), Data (" + columnLine + ")");
+                    record = new ProducerRecord(fiwareServicePaths[i], columnLine);
+                    break;
+                case TOPICBYSERVICE:
+                    LOGGER.info("[" + this.getName() + "] Persisting data at OrionKafkaSink. Topic ("
+                            + fiwareService + "), Data (" + columnLine + ")");
+                    record = new ProducerRecord(fiwareService, columnLine);
+                    break;
+                default:
+                    break;
+            } // switch
+            
+            persistenceBackend.send(record);
         } // for
     } // persist
 
