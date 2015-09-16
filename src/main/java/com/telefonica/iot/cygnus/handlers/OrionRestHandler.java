df14dbb66a175bd17ecc43d75b710894a1147e97
frbattid <francisco.romerobueno@telefonica.com>
2015-09-16 12:16:08 +0200

    ADD debug logs regarding the notified headers

--- src/main/java/com/telefonica/iot/cygnus/handlers/OrionRestHandler.java
+++ src/main/java/com/telefonica/iot/cygnus/handlers/OrionRestHandler.java
@@ -174,6 +174,7 @@ public class OrionRestHandler implements HTTPSourceHandler {
         while (headerNames.hasMoreElements()) {
             String headerName = ((String) headerNames.nextElement()).toLowerCase(Locale.ENGLISH);
             String headerValue = request.getHeader(headerName).toLowerCase(Locale.ENGLISH);
+            LOGGER.debug("Header " + headerName + " received with value " + headerValue);
             
             if (headerName.equals(Constants.HEADER_CONTENT_TYPE)) {
                 if (!headerValue.contains("application/json") && !headerValue.contains("application/xml")) {
