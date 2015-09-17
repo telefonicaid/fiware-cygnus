42a4be0b9c65baf141281d194ab26f9fc94e2b5c
frbattid <francisco.romerobueno@telefonica.com>
2015-09-17 15:57:49 +0200

    ADD hiveserver1-client implementation for node.js

---
+++ resources/hiveclients/nodejs/hiveserver1-client/src/hiveserver1-client.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-cygnus (FI-WARE project).
+ *
+ * fiware-cygnus is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * fiware-cygnus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with fiware-cygnus. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please contact with iot_support at tid dot es
+ */
+
+// dependencies
+var hive = require('thrift-hive');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+
+// get a connection
+var client = hive.createClient({
+    version: '0.7.1-cdh3u2',
+    server: 'cosmos.lab.fiware.org',
+    port: 9999,
+    timeout: 1000
+});
+
+// loop prompting for HiveQL sentences
+function doLoop() {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write('remotehive> ');
+
+    process.stdin.on('data', function (query) {
+        if (query === 'exit\n') {
+            client.end();
+            process.exit();
+        } // if
+
+        // use the given database
+        client.execute('use default', function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            } // if
+
+            // do the query
+            client.query(query)
+                .on('row', function (database) {
+                    console.log(database);
+                })
+                .on('error', function (err) {
+                    console.log(err.message);
+                    doLoop();
+                })
+                .on('end', function () {
+                    doLoop();
+                });
+        });
+    });
+} // doLoop
+
+// do the loop
+doLoop();
+
