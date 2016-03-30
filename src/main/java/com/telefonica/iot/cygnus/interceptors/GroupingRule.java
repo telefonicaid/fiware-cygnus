26c39c669eb3e6ded1eeddca9999b9a2d6f14909
frbattid <francisco.romerobueno@telefonica.com>
2016-03-30 17:21:42 +0200

    ADD an extra check when validating a rule, if the service path starts with slash

--- src/main/java/com/telefonica/iot/cygnus/interceptors/GroupingRule.java
+++ src/main/java/com/telefonica/iot/cygnus/interceptors/GroupingRule.java
@@ -111,6 +111,7 @@ public class GroupingRule {
         boolean containsDestination = false;
         boolean containsFiwareServicePath = false;
         boolean containsExtraFields = false;
+        boolean servicePathStartsWithSlash = false;
         int fieldsSize = 0;
         int regexLength = 0;
         int destinationLength = 0;
@@ -133,6 +134,7 @@ public class GroupingRule {
                 destinationLength = ((String) jsonRule.get("destination")).length();
             } else if (field.equals("fiware_service_path")) {
                 containsFiwareServicePath = true;
+                servicePathStartsWithSlash = field.startsWith("/");
                 fiwareServicePathLength = ((String) jsonRule.get("fiware_service_path")).length();
             } else {
                 containsExtraFields = true;
@@ -152,6 +154,11 @@ public class GroupingRule {
         // check if the rule has extra fields not allowed
         if (checkExtraFields && containsExtraFields) {
             return 3;
+        } // if
+        
+        // check if the service-path starts with '/'
+        if (!servicePathStartsWithSlash) {
+            return 4;
         } // if
 
         return 0;
