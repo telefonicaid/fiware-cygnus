df6ac3c074d66c2a8f4b0416cf4925785bb647eb
frbattid <francisco.romerobueno@telefonica.com>
2016-04-05 11:33:03 +0200

    ADD tests to OrionTest about the limits of the parameters

--- src/test/java/com/telefonica/iot/cygnus/sinks/OrionSinkTest.java
+++ src/test/java/com/telefonica/iot/cygnus/sinks/OrionSinkTest.java
@@ -22,6 +22,7 @@ import org.apache.flume.Context;
 import org.apache.flume.channel.MemoryChannel;
 import org.apache.flume.lifecycle.LifecycleState;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -49,7 +50,7 @@ public class OrionSinkTest {
     public void testStart() {
         System.out.println("[OrionSink] -------- The sink starts properly");
         OrionSinkImpl sink = new OrionSinkImpl();
-        sink.configure(createContext());
+        sink.configure(createContext(null, null, null, null, null, null)); // default configuration
         sink.setChannel(new MemoryChannel());
         sink.start();
         LifecycleState state = sink.getLifecycleState();
@@ -73,7 +74,7 @@ public class OrionSinkTest {
         System.out.println("[OrionSink.configure] -------- When not configured, the default values are used "
                 + "for non mandatory parameters");
         OrionSinkImpl sink = new OrionSinkImpl();
-        sink.configure(createContext());
+        sink.configure(createContext(null, null, null, null, null, null)); // default configuration
         
         try {
             assertEquals(1, sink.getBatchSize());
@@ -136,14 +137,109 @@ public class OrionSinkTest {
         } // try catch
     } // testConfigureNotMandatoryParameters
     
-    private Context createContext() {
+    /**
+     * [OrionSink.configure] -------- The configuration becomes invalid upon out-of-the-limits configured values
+     * for parameters having a discrete set of accepted values, or numerical values having upper or lower limits.
+     */
+    @Test
+    public void testConfigureInvalidConfiguration() {
+        System.out.println("[OrionSink.configure] -------- The configuration becomes invalid upon out-of-the-limits "
+                + "configured values for parameters having a discrete set of accepted values, or numerical values "
+                + "having upper or lower limits");
+        OrionSinkImpl sink = new OrionSinkImpl();
+        String configuredBatchSize = "0";
+        sink.configure(createContext(configuredBatchSize, null, null, null, null, null));
+        
+        try {
+            assertTrue(sink.getInvalidConfiguration());
+            System.out.println("[OrionSink.configure] -  OK  - A wrong configuration 'batch_size='"
+                    + configuredBatchSize + "' has been detected");
+        } catch (AssertionError e) {
+            System.out.println("[OrionSink.configure] - FAIL - A wrong configuration 'batch_size='"
+                    + configuredBatchSize + "' has not been detected");
+            throw e;
+        } // try catch
+        
+        sink = new OrionSinkImpl();
+        String configuredBatchTimeout = "0";
+        sink.configure(createContext(null, configuredBatchTimeout, null, null, null, null));
+        
+        try {
+            assertTrue(sink.getInvalidConfiguration());
+            System.out.println("[OrionSink.configure] -  OK  - A wrong configuration 'batch_timeout='"
+                    + configuredBatchTimeout + "' has been detected");
+        } catch (AssertionError e) {
+            System.out.println("[OrionSink.configure] - FAIL - A wrong configuration 'batch_timeout='"
+                    + configuredBatchTimeout + "' has not been detected");
+            throw e;
+        } // try catch
+        
+        sink = new OrionSinkImpl();
+        String configuredBatchTTL = "-2";
+        sink.configure(createContext(null, null, configuredBatchTTL, null, null, null));
+        
+        try {
+            assertTrue(sink.getInvalidConfiguration());
+            System.out.println("[OrionSink.configure] -  OK  - A wrong configuration 'batch_ttl='"
+                    + configuredBatchTTL + "' has been detected");
+        } catch (AssertionError e) {
+            System.out.println("[OrionSink.configure] - FAIL - A wrong configuration 'batch_ttl='"
+                    + configuredBatchTTL + "' has not been detected");
+            throw e;
+        } // try catch
+        
+        sink = new OrionSinkImpl();
+        String dataModel = "dm-by-other";
+        sink.configure(createContext(null, null, null, dataModel, null, null));
+        
+        try {
+            assertTrue(sink.getInvalidConfiguration());
+            System.out.println("[OrionSink.configure] -  OK  - A wrong configuration 'data_model='"
+                    + dataModel + "' has been detected");
+        } catch (AssertionError e) {
+            System.out.println("[OrionSink.configure] - FAIL - A wrong configuration 'data_model='"
+                    + dataModel + "' has not been detected");
+            throw e;
+        } // try catch
+        
+        sink = new OrionSinkImpl();
+        String configuredEnableGrouping = "falso";
+        sink.configure(createContext(null, null, null, null, configuredEnableGrouping, null));
+        
+        try {
+            assertTrue(sink.getInvalidConfiguration());
+            System.out.println("[OrionSink.configure] -  OK  - A wrong configuration 'enable_grouping='"
+                    + configuredEnableGrouping + "' has been detected");
+        } catch (AssertionError e) {
+            System.out.println("[OrionSink.configure] - FAIL - A wrong configuration 'enable_grouping='"
+                    + configuredEnableGrouping + "' has not been detected");
+            throw e;
+        } // try catch
+        
+        sink = new OrionSinkImpl();
+        String configuredEnableLowercase = "verdadero";
+        sink.configure(createContext(null, null, null, null, null, configuredEnableLowercase));
+        
+        try {
+            assertTrue(sink.getInvalidConfiguration());
+            System.out.println("[OrionSink.configure] -  OK  - A wrong configuration 'enable_lowercase='"
+                    + configuredEnableLowercase + "' has been detected");
+        } catch (AssertionError e) {
+            System.out.println("[OrionSink.configure] - FAIL - A wrong configuration 'enable_lowercase='"
+                    + configuredEnableLowercase + "' has not been detected");
+            throw e;
+        } // try catch
+    } // testConfigureInvalidConfiguration
+    
+    private Context createContext(String batchSize, String batchTimeout, String batchTTL, String dataModel,
+            String enableGrouping, String enableLowercase) {
         Context context = new Context();
-        context.put("batch_size", "1");
-        context.put("batch_timeout", "30");
-        context.put("batch_ttl", "10");
-        context.put("data_model", "dm-by-entity");
-        context.put("enable_grouping", "false");
-        context.put("enable_lowercase", "false");
+        context.put("batch_size", batchSize);
+        context.put("batch_timeout", batchTimeout);
+        context.put("batch_ttl", batchTTL);
+        context.put("data_model", dataModel);
+        context.put("enable_grouping", enableGrouping);
+        context.put("enable_lowercase", enableLowercase);
         return context;
     } // createContext
     
