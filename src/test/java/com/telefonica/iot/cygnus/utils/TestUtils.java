fcf10903688dd94edd8a0cf50bfda3ce5083c690
frbattid <francisco.romerobueno@telefonica.com>
2016-04-08 13:08:45 +0200

    ADD several utility functions for testing

--- src/test/java/com/telefonica/iot/cygnus/utils/TestUtils.java
+++ src/test/java/com/telefonica/iot/cygnus/utils/TestUtils.java
@@ -20,8 +20,14 @@ package com.telefonica.iot.cygnus.utils;
 
 import com.google.gson.Gson;
 import com.telefonica.iot.cygnus.containers.NotifyContextRequest;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.lang.StringUtils;
+import org.apache.flume.Event;
+import org.apache.flume.event.EventBuilder;
 import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -66,4 +72,42 @@ public final class TestUtils {
         traceHead += " " + StringUtils.repeat("-", MAX_LEN_TEST_TRACE_HEAD - originalHead.length());
         return traceHead;
     } // getTestTraceHead
+    
+    public static Event createEvent() {
+        String eventData = createNotification().toJSONString();
+        Map<String, String> eventHeaders = new HashMap<String, String>();
+        eventHeaders.put("fiware-service", "default");
+        eventHeaders.put("fiware-servicepath", "/");
+        eventHeaders.put("fiware-correlator", "0123456789ABCDEF");
+        eventHeaders.put("transaction-id", "0123456789ABCDEF");
+        return EventBuilder.withBody(eventData.getBytes(), eventHeaders);
+    } // createEvent
+    
+    public static JSONObject createNotification() {
+        JSONObject attribute = new JSONObject();
+        attribute.put("name", "temperature");
+        attribute.put("type", "centigrade");
+        attribute.put("value", "26.5");
+        JSONArray attributes = new JSONArray();
+        attributes.add(attribute);
+        JSONObject contextElement = new JSONObject();
+        contextElement.put("attributes", attributes);
+        contextElement.put("type", "Room");
+        contextElement.put("isPattern", "false");
+        contextElement.put("id", "room1");
+        JSONObject statusCode = new JSONObject();
+        statusCode.put("code", "200");
+        statusCode.put("reasonPhrase", "OK");
+        JSONObject contextResponse = new JSONObject();
+        contextResponse.put("contextElement", contextElement);
+        contextResponse.put("statusCode", statusCode);
+        JSONArray contextResponses = new JSONArray();
+        contextResponses.add(contextResponse);
+        JSONObject notification = new JSONObject();
+        notification.put("subscriptionId", "51c0ac9ed714fb3b37d7d5a8");
+        notification.put("originator", "localhost");
+        notification.put("contextResponses", contextResponses);
+        return notification;
+    } // createNotification
+    
 } // TestUtils
