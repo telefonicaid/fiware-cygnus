169796069c9044ced79812341533abe082a00eb3
Víctor García Guillén <vgarcia@tcpsi.es>
2016-04-12 14:39:06 +0200

    changed glob expressions

--- cygnus-ngsi/neore/rpm/get_version_string.sh
+++ cygnus-ngsi/neore/rpm/get_version_string.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright 2014 Telefonica Investigacion y Desarrollo, S.A.U
 #
@@ -23,7 +23,11 @@
 # Bash lib to know the RPM version and revision from a Github repository
 # Call method get_rpm_version_string to obtain them for rpmbuild
 
-#shopt -s extglob
+if [[ $(ps -hp  $$ | grep bash) ]]; then
+  shopt -s extglob
+elif [[ $(ps -hp  $$ | grep zsh) ]]; then
+  setopt kshglob
+fi
 
 get_branch()
 {
@@ -48,37 +52,28 @@ get_version_string()
     case $(get_branch_type) in
         stable)
            # If we are on stable branch get last tag as the version, but transform to x.x.x-x-SHA1
-           describe_tags="$(git describe --tags --long  --match "[0-9][0-9].[0-9][0-9].[0-9][0-9]" 2>/dev/null)"
-           [[ ${?} -ne 0 ]] && describe_tags="$(git describe --tags --long  --match "releae-[0-9][0-9].[0-9][0-9].[0-9][0-9]" 2>/dev/null)"
+           describe_tags="$(git describe --tags --long  --match "[[:digit:]]*.[[:digit:]]*.[[:digit:]]*" 2>/dev/null)"
            version="${describe_tags%-*-*}"
-           version="${version#*release-}"
            echo "${version%.*}-${version#*.*.*.}-$(git log --pretty=format:'%h' -1)"
         ;;
         develop)
           ## If we are in develop use the total count of commits of the repo
           total_commit_number=$(git rev-list --all --count)
           short_hash=$(git rev-parse --short HEAD)
-          version="$(git describe --tags --long  --match "[0-9][0-9].[0-9][0-9].[0-9][0-9]" 2>/dev/null)"
-          [[ ${?} -ne 0 ]] && version="$(git describe --tags --long  --match "release-[0-9][0-9].[0-9][0-9].[0-9][0-9]" 2>/dev/null)"
-          version="${version%-*-*}"
-          version="${version#*release-}"
-          echo "${version}-${total_commit_number}-${short_hash}"
+          version="$(git describe --tags --long  --match "[[:digit:]]*.[[:digit:]]*.[[:digit:]]*" 2>/dev/null)"
+          echo "${version%-*-*}-${total_commit_number}-${short_hash}"
         ;;
         release)
           ## in release branches the version is a tag named
           branch_name="$(get_branch)"
           branch_name="${branch_name#*/}"
           version="$(git describe --tags --long  --match ${branch_name} 2>/dev/null)"
-          [[ ${?} -ne 0 ]] && version="$(git describe --tags --long  --match "release-${branch_name}" 2>/dev/null)"
           echo ${version}
         ;;
         other)
-          ## We are in detached mode, use the last x-y-z tag
-          version="$(git describe --tags --long  --match "[0-9][0-9].[0-9][0-9].[0-9][0-9]" 2>/dev/null)"
-          [[ ${?} -ne 0 ]] && version="$(git describe --tags --long  --match "release-[0-9][0-9].[0-9][0-9].[0-9][0-9]" 2>/dev/null)"
-          version="${version%-*-*}"
-          version="${version#*release-}"
-          echo "${version}"
+            ## We are in detached mode, use the last x-y-z tag
+            version="$(git describe --tags --long  --match "[[:digit:]]*.[[:digit:]]*.[[:digit:]]*" 2>/dev/null)"
+            echo "${version%-*-*}"
         ;;
         *)
            # RMs don't stablish any standard here, we use branch name as version
