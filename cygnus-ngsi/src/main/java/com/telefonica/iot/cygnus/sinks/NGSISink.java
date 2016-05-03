92a7d83c18fbad051b300f34e395992ad30319ed
frbattid <francisco.romerobueno@telefonica.com>
2016-05-03 16:49:33 +0200

    [898][cygnus-ngsi][NGSISink.java] Fix the per-attribute accumulation of events

--- cygnus-ngsi/src/main/java/com/telefonica/iot/cygnus/sinks/NGSISink.java
+++ cygnus-ngsi/src/main/java/com/telefonica/iot/cygnus/sinks/NGSISink.java
@@ -645,7 +645,7 @@ public abstract class NGSISink extends CygnusSink implements Configurable {
                                 + "_" + attr.getName();
                         NGSIEvent cygnusEvent = new NGSIEvent(
                                 recvTimeTs, service, notifiedServicePaths[i], notifiedEntities[i],
-                                attr.getName(), contextElement.filter(destination));
+                                attr.getName(), contextElement.filter(attr.getName()));
                         batch.addEvent(destination, cygnusEvent);
                     } // for
                 } // for
