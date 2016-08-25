316c98b86f492d834cc9981861b3f472d34f9687
frbattid <francisco.romerobueno@telefonica.com>
2016-08-25 10:53:18 +0200

    [1107][CommonConstants.java] Ipdate the concatenator, from x0000 to xffff

--- cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonConstants.java
+++ cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonConstants.java
@@ -69,30 +69,30 @@ public final class CommonConstants {
     
     // encoding
     public static final String INTERNAL_CONCATENATOR = "=";
-    public static final String CONCATENATOR = "x0000";
+    public static final String CONCATENATOR = "xffff";
     
     // Header for API uses
-    public static final String CYGNUS_IPR_HEADER = "#####\n"  
-                                    + "# Copyright 2016 Telefonica Investigación y Desarrollo, S.A.U\n" 
-                                    + "# \n" 
-                                    + "# This file is part of fiware-cygnus (FI-WARE project).\n" 
-                                    + "# \n" 
-                                    + "# fiware-cygnus is free software: you can redistribute it and/or modify it under "
-                                    + "the terms of the GNU Affero General\n" 
-                                    + "# Public License as published by the Free Software Foundation, either version 3 "
-                                    + "of the License, or (at your option) any\n" 
-                                    + "# later version.\n" 
-                                    + "# fiware-cygnus is distributed in the hope that it will be useful, but WITHOUT "
-                                    + "ANY WARRANTY; without even the implied\n" 
-                                    + "# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU "
-                                    + "Affero General Public License for more\n" 
-                                    + "# details.\n" 
-                                    + "# \n" 
-                                    + "# You should have received a copy of the GNU Affero General Public License "
-                                    + "along with fiware-cygnus. If not, see\n" 
-                                    + "# http://www.gnu.org/licenses/.\n" 
-                                    + "# \n" 
-                                    + "# For those usages not covered by the GNU Affero General Public License please "
-                                    + "contact with iot_support at tid dot es";
+    public static final String CYGNUS_IPR_HEADER = "#####\n"
+            + "# Copyright 2016 Telefonica Investigación y Desarrollo, S.A.U\n"
+            + "# \n"
+            + "# This file is part of fiware-cygnus (FI-WARE project).\n"
+            + "# \n"
+            + "# fiware-cygnus is free software: you can redistribute it and/or modify it under "
+            + "the terms of the GNU Affero General\n"
+            + "# Public License as published by the Free Software Foundation, either version 3 "
+            + "of the License, or (at your option) any\n"
+            + "# later version.\n"
+            + "# fiware-cygnus is distributed in the hope that it will be useful, but WITHOUT "
+            + "ANY WARRANTY; without even the implied\n"
+            + "# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU "
+            + "Affero General Public License for more\n"
+            + "# details.\n"
+            + "# \n"
+            + "# You should have received a copy of the GNU Affero General Public License "
+            + "along with fiware-cygnus. If not, see\n"
+            + "# http://www.gnu.org/licenses/.\n"
+            + "# \n"
+            + "# For those usages not covered by the GNU Affero General Public License please "
+            + "contact with iot_support at tid dot es";
     
 } // CommonConstants
