5afe5764d7bf27f7785515f8db2736a7f8b674d4
frbattid <francisco.romerobueno@telefonica.com>
2016-08-25 10:57:03 +0200

    [1107][CommonUtilsForTests.java] Add enableEncoding as a parameter of MongoDB/STH Flume Context

--- cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonUtilsForTests.java
+++ cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonUtilsForTests.java
@@ -100,9 +100,12 @@ public final class CommonUtilsForTests {
      * Creates a Flume context for Mongo/STH sinks.
      * @param collectionPrefix
      * @param dbPrefix
+     * @param dataModel
+     * @param enableEncoding
      * @return A Flume context for Mongo/STH sinks.
      */
-    public static Context createContextForMongoSTH(String collectionPrefix, String dbPrefix, String dataModel) {
+    public static Context createContextForMongoSTH(String collectionPrefix, String dbPrefix, String dataModel,
+            String enableEncoding) {
         Context context = new Context();
         context.put("attr_persistence", "row");
         context.put("batch_size", "100");
@@ -113,6 +116,7 @@ public final class CommonUtilsForTests {
         context.put("data_expiration", "0");
         context.put("data_model", dataModel);
         context.put("db_prefix", dbPrefix);
+        context.put("enable_encoding", enableEncoding);
         context.put("enable_grouping", "false");
         context.put("enable_lowercase", "false");
         context.put("max_documents", "0");
