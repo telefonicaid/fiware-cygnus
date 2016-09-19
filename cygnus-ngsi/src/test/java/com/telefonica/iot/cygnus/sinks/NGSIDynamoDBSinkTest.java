761e6a994b4197457caaada5fcaf230111eec56a
frbattid <francisco.romerobueno@telefonica.com>
2016-09-19 12:31:07 +0200

    [1152][cygnus-ngsi][NGSIDynamoDBSinkTest.java] Add

--- cygnus-ngsi/src/test/java/com/telefonica/iot/cygnus/sinks/NGSIDynamoDBSinkTest.java
+++ cygnus-ngsi/src/test/java/com/telefonica/iot/cygnus/sinks/NGSIDynamoDBSinkTest.java
@@ -17,8 +17,10 @@
  */
 package com.telefonica.iot.cygnus.sinks;
 
-import com.telefonica.iot.cygnus.containers.NotifyContextRequest;
+import static com.telefonica.iot.cygnus.utils.CommonUtilsForTests.getTestTraceHead;
 import org.apache.flume.Context;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -27,28 +29,132 @@ import org.junit.Test;
  */
 public class NGSIDynamoDBSinkTest {
     
+    /**
+     * [NGSIDynamoDBSink.configure] -------- When not configured, not mandatory parameters get default values.
+     */
     @Test
-    public void dummyTest() {
-    } // dummyTest
+    public void testConfigureDefaults() {
+        System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                + "-------- When not configured, not mandatory parameters get default values");
+        String accessKeyId = "my_access_key";
+        String attrPersistence = null;
+        String batchSize = null;
+        String batchTimeout = null;
+        String batchTTL = null;
+        String dataModel = null;
+        String enableGrouping = null;
+        String enableLowercase = null;
+        String region = null;
+        String secretAccessKey = "my_secret_access_key";
+        NGSIDynamoDBSink sink = new NGSIDynamoDBSink();
+        sink.configure(createContext(accessKeyId, attrPersistence, batchSize, batchTimeout, batchTTL, dataModel,
+                enableGrouping, enableLowercase, region, secretAccessKey));
+        
+        try {
+            assertTrue(sink.getRowAttrPersistence());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'attr_persisnce=row' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'attr_persisnce=row' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertEquals(1, sink.getBatchSize());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'batch_size=1' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'batch_size=1' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertEquals(30, sink.getBatchTimeout());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'batch_timeout=30' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'batch_timeout=30' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertEquals(10, sink.getBatchTTL());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'batch_ttl=30' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'batch_ttl=30' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertEquals(Enums.DataModel.DMBYENTITY, sink.getDataModel());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'data_model=dm-by-entity' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'data_model=dm-by-entity' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertTrue(sink.getEnableEncoding());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'enable_encoding=true' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'enable_encoding=true' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertTrue(!sink.getEnableGrouping());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'enable_grouping=false' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'enable_grouping=false' not configured by default");
+            throw e;
+        } // try catch
+        
+        try {
+            assertTrue(!sink.getEnableLowerCase());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'enable_lowercase=false' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'enable_lowercase=false' not configured by default");
+            throw e;
+        } // try catch
+
+        try {
+            assertEquals("eu-west-1", sink.getRegion());
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "-  OK  - 'region=eu-west-1' configured by default");
+        } catch (AssertionError e) {
+            System.out.println(getTestTraceHead("[NGSIDynamoDBSink.configure]")
+                    + "- FAIL - 'region=eu-west-1' not configured by default");
+            throw e;
+        } // try catch
+    } // testConfigureDefaults
     
-    private NGSIBatch createBatch(long recvTimeTs, String service, String servicePath, String destination,
-            NotifyContextRequest.ContextElement contextElement) {
-        NGSIEvent groupedEvent = new NGSIEvent(recvTimeTs, service, servicePath, destination, null,
-            contextElement);
-        NGSIBatch batch = new NGSIBatch();
-        batch.addEvent(destination, groupedEvent);
-        return batch;
-    } // createBatch
-    
-    private Context createContext(String accessKeyId, String attrPersistence, String enableGrouping, String region,
-            String secretAccessKey, String tableType) {
+    private Context createContext(String accessKeyId, String attrPersistence, String batchSize, String batchTimeout,
+            String batchTTL, String dataModel, String enableGrouping, String enableLowercase, String region,
+            String secretAccessKey) {
         Context context = new Context();
         context.put("access_key_id", accessKeyId);
         context.put("attr_persistence", attrPersistence);
+        context.put("batch_size", batchSize);
+        context.put("batch_timeout", batchTimeout);
+        context.put("batch_ttl", batchTTL);
+        context.put("data_model", dataModel);
         context.put("enable_grouping", enableGrouping);
+        context.put("enable_lowercase", enableLowercase);
         context.put("region", region);
         context.put("secret_access_key", secretAccessKey);
-        context.put("table_type", tableType);
         return context;
     } // createContext
     
