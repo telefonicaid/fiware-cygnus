a81a5f0e605a6444b7e98034f52c127e7b81d69e
frbattid <francisco.romerobueno@telefonica.com>
2016-10-05 13:51:28 +0200

    [1171][cygnus-common][PostgreSQLCache] Fix iteration loops

--- cygnus-common/src/main/java/com/telefonica/iot/cygnus/backends/postgresql/PostgreSQLCache.java
+++ cygnus-common/src/main/java/com/telefonica/iot/cygnus/backends/postgresql/PostgreSQLCache.java
@@ -28,31 +28,32 @@ import java.util.HashMap;
  */
 public class PostgreSQLCache {
     
-    private final CygnusLogger LOGGER = new CygnusLogger(PostgreSQLCache.class);
+    private static final CygnusLogger LOGGER = new CygnusLogger(PostgreSQLCache.class);
     private HashMap<String, ArrayList<String>> cache = new HashMap<String, ArrayList<String>>();
     
     public HashMap<String, ArrayList<String>> getCache() {
         return cache;
     } // getCache
     
-    public void setCache(HashMap<String, ArrayList<String>> PSQLcache) {
-        this.cache = PSQLcache;
+    public void setCache(HashMap<String, ArrayList<String>> cache) {
+        this.cache = cache;
     } // setCache
     
-    public boolean isSchemaInCache (String schemaName) {    
+    /**
+     * Gets if the schema is in the cache.
+     * @param schemaName
+     * @return True if the schema is in the cache, otherwise false
+     */
+    public boolean isSchemaInCache(String schemaName) {
         LOGGER.debug("Checking if the schema (" + schemaName + ") exists");
         if (!cache.isEmpty()) {
-            
-            for (HashMap.Entry<String,ArrayList<String>> entry : cache.entrySet()) {
-                String schema = entry.getKey();
-                
+            for (String schema : cache.keySet()) {
                 if (schema.equals(schemaName)) {
                     LOGGER.debug("Schema (" + schemaName + ") exists in Cache");
                     return true;
                 } // if
-                
             } // for
-            
+
             LOGGER.debug("Schema (" + schemaName + ") doesnt' exist in Cache");
             return false;
         } else {
@@ -62,55 +63,64 @@ public class PostgreSQLCache {
 
     } // isSchemaInCache
     
-    public boolean isTableInCachedSchema (String schemaName, String tableName) {
+    /**
+     * Gets if a table within a shema is in the cache.
+     * @param schemaName
+     * @param tableName
+     * @return True if the table within the shema is in the cache, otherwise false
+     */
+    public boolean isTableInCachedSchema(String schemaName, String tableName) {
         if (!cache.isEmpty()) {
             
-            for (HashMap.Entry<String,ArrayList<String>> entry : cache.entrySet()) {
-                String schema = entry.getKey();
-                
+            for (String schema : cache.keySet()) {
                 if (schema.equals(schemaName)) {
                     ArrayList<String> tables = cache.get(schemaName);
 
                     LOGGER.info("Checking if the table (" + tableName + ") belongs to (" + schemaName + ")");
 
                     if (tables.contains(tableName)) {
-                        LOGGER.debug("Table (" + tableName + ") was found in the specified schema (" + schemaName + ")");
+                        LOGGER.debug("Table (" + tableName + ") was found in the specified schema ("
+                                + schemaName + ")");
                         return true;
                     } else {
-                        LOGGER.debug("Table (" + tableName + ") wasn't found in the specified schema (" + schemaName + ")");
+                        LOGGER.debug("Table (" + tableName + ") wasn't found in the specified schema ("
+                                + schemaName + ")");
                         return false;
                     } // if else
-                
-                } // if     
+                } // if
             } // for
             
             LOGGER.debug("Schema (" + schemaName + ") wasn't found in Cache");
-            return false;            
+            return false;
         } else {
             LOGGER.debug("Cache is empty");
             return false;
         }
     } // isTableInCachedSchema
     
+    /**
+     * Adds a schema to the cache.
+     * @param schemaName
+     */
     public void persistSchemaInCache(String schemaName) {
         cache.put(schemaName, new ArrayList<String>());
         LOGGER.debug("Schema (" + schemaName + ") added to cache");
     } // persistSchemaInCache
     
+    /**
+     * Adds a table within a schema to the cache.
+     * @param schemaName
+     * @param tableName
+     */
     public void persistTableInCache(String schemaName, String tableName) {
-        
-        for (HashMap.Entry<String,ArrayList<String>> entry : cache.entrySet()) {
-                String schema = entry.getKey();
-                
-                if (schema.equals(schemaName)) {
-                    ArrayList<String> tableNames = cache.get(schemaName);
-                    tableNames.add(tableName);
-                    cache.put(schemaName, tableNames);
-                    LOGGER.debug("Table (" + tableName + ") added to schema (" + schemaName + ") in cache");
-                } // if 
-                
-            } // for
-        
+        for (String schema : cache.keySet()) {
+            if (schema.equals(schemaName)) {
+                ArrayList<String> tableNames = cache.get(schemaName);
+                tableNames.add(tableName);
+                cache.put(schemaName, tableNames);
+                LOGGER.debug("Table (" + tableName + ") added to schema (" + schemaName + ") in cache");
+            } // if
+        } // for
     } // persistTableInCache
     
 } // PostgreSQLCache
