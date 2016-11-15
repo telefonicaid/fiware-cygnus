436803b93eddcc193974c2eb39579b3393d1016c
pcoello25 <pablo.coellovillalba@telefonica.com>
2016-11-15 09:57:43 +0100

    [1299][cygnus] Add path to conf file

--- docker/cygnus-ngsi/cygnus-entrypoint.sh
+++ docker/cygnus-ngsi/cygnus-entrypoint.sh
@@ -40,6 +40,7 @@ sed -i '/'${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.hdfs_host/c '${CYGNUS_AGENT_NAME
 sed -i '/'${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.hdfs_port/c '${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.hdfs_port = '${CYGNUS_HDFS_PORT} ${FLUME_HOME}/conf/agent.conf
 sed -i '/'${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.hdfs_username/c '${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.hdfs_username = '${CYGNUS_HDFS_USER} ${FLUME_HOME}/conf/agent.conf
 sed -i '/'${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.oauth2_token/c '${CYGNUS_AGENT_NAME}'.sinks.hdfs-sink.oauth2_token = '${CYGNUS_HDFS_TOKEN} ${FLUME_HOME}/conf/agent.conf
+sed -i '/'${CYGNUS_AGENT_NAME}'.sinks.cartodb-sink.keys_conf_file/c '${CYGNUS_AGENT_NAME}'.sinks.cartodb-sink.keys_conf_file = '${CYGNUS_CARTO_KEY_CONF_FILE} ${FLUME_HOME}/conf/agent.conf
 
 # Change parameter in the cartodb key configuration file
 sed -i '/"username":/c "username":"'${CYGNUS_CARTO_USER}'",' ${FLUME_HOME}/conf/cartodb_keys.conf
