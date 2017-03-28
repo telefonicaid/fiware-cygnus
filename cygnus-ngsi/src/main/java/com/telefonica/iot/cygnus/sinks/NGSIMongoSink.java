eea57390e63e1bcf8c778877adbf4833fb357df4
sabrine <sabrine.fatnassi@eglobalmark.com>
2017-03-28 11:05:05 +0200

    Revert "applicate indentation patch "
    This reverts commit 115c0b6c41d779f1dac2f76d301974a7d295c864.

--- cygnus-ngsi/src/main/java/com/telefonica/iot/cygnus/sinks/NGSIMongoSink.java
+++ cygnus-ngsi/src/main/java/com/telefonica/iot/cygnus/sinks/NGSIMongoSink.java
@@ -86,15 +86,14 @@ public class NGSIMongoSink extends NGSIMongoBaseSink {
         }  // if else
         
         attrMetadataStore = context.getString("attr_metadata_store", "false");
-
-        if (attrMetadataStore.equals("true") || attrMetadataStore.equals("false")) {
-            LOGGER.debug("[" + this.getName() + "] Reading configuration (attr_metadata_store="
-                    + attrMetadataStore + ")");
-        } else {
-            invalidConfiguration = true;
-            LOGGER.debug("[" + this.getName() + "] Invalid configuration (attr_metadata_store="
-                    + attrMetadataStore + ") must be 'true' or 'false'");
-        } // if else 
+		if (attrMetadataStore.equals("true") || attrMetadataStore.equals("false")) {        	
+			LOGGER.debug("[" + this.getName() + "] Reading configuration (attr_metadata_store="
+					+ attrMetadataStore + ")");
+		} else {
+			invalidConfiguration = true;
+			LOGGER.debug("[" + this.getName() + "] Invalid configuration (attr_metadata_store="
+					+ attrMetadataStore + ") must be 'true' or 'false'");
+		} 
 		
         super.configure(context);
     } // configure
@@ -250,47 +249,48 @@ public class NGSIMongoSink extends NGSIMongoBaseSink {
                         + attrType + ")");
                 
                 Document doc;
-
-                if(attrMetadataStore.equals("true")){
-                    doc = createDocWithMetadata(recvTimeTs, entityId, entityType, attrName, attrType, attrValue, attrMetadata);
-                } else {
-                    doc = createDoc(recvTimeTs, entityId, entityType, attrName, attrType, attrValue);
-                } // if else
-		                
+				if(attrMetadataStore.equals("true")){
+					doc = createDocWithMetadata(recvTimeTs, entityId, entityType, attrName, attrType, attrValue, attrMetadata);
+				}
+				else{
+					doc = createDoc(recvTimeTs, entityId, entityType, attrName, attrType, attrValue);
+				}
+				                
                 aggregation.add(doc);
             } // for
         } // aggregate
         
         private Document createDocWithMetadata(Long recvTimeTs, String entityId, String entityType, String attrName,
-                String attrType, String attrValue, String attrMetadata) {
-            Document doc = new Document("recvTime", new Date(recvTimeTs));
+				String attrType, String attrValue, String attrMetadata) {
 
-            switch (dataModel) {
-            case DMBYSERVICEPATH:
-                doc.append("entityId", entityId)
-                        .append("entityType", entityType)
-                        .append("attrName", attrName)
-                        .append("attrType", attrType)
-                        .append("attrValue", attrValue)
-                        .append("attrMetadata", (DBObject)JSON.parse(attrMetadata));
-                break;
-            case DMBYENTITY:
-                doc.append("attrName", attrName)
-                        .append("attrType", attrType)
-                        .append("attrValue", attrValue)
-                        .append("attrMetadata", (DBObject)JSON.parse(attrMetadata));
-                break;
-            case DMBYATTRIBUTE:
-                doc.append("attrType", attrType)
-                        .append("attrValue", attrValue)
-                        .append("attrMetadata", (DBObject)JSON.parse(attrMetadata));
-                break;
-            default:
-                return null; // this will never be reached
-            } // switch
+			Document doc = new Document("recvTime", new Date(recvTimeTs));
 
-            return doc;			
-        } // createDocWithMetadata
+			switch (dataModel) {
+			case DMBYSERVICEPATH:
+				doc.append("entityId", entityId)
+				.append("entityType", entityType)
+				.append("attrName", attrName)
+				.append("attrType", attrType)
+				.append("attrValue", attrValue)
+				.append("attrMetadata", (DBObject)JSON.parse(attrMetadata));
+				break;
+			case DMBYENTITY:
+				doc.append("attrName", attrName)
+				.append("attrType", attrType)
+				.append("attrValue", attrValue)
+				.append("attrMetadata", (DBObject)JSON.parse(attrMetadata));
+				break;
+			case DMBYATTRIBUTE:
+				doc.append("attrType", attrType)
+				.append("attrValue", attrValue)
+				.append("attrMetadata", (DBObject)JSON.parse(attrMetadata));
+				break;
+			default:
+				return null; // this will never be reached
+			} // switch
+
+			return doc;			
+		}// createDocWithMetadata
         
         private Document createDoc(long recvTimeTs, String entityId, String entityType, String attrName,
                 String attrType, String attrValue) {
@@ -419,7 +419,7 @@ public class NGSIMongoSink extends NGSIMongoBaseSink {
             backend.insertContextDataRaw(dbName, collectionName, aggregation);
         } catch (Exception e) {
             throw new CygnusPersistenceError("-, " + e.getMessage());
-        } // try catch
+        }
     } // persistAggregation
 
 } // NGSIMongoSink
