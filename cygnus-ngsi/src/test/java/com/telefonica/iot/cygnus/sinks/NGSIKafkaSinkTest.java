a753feffa9e99255707b965bb1dca4acec3fa653
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2018-04-30 17:01:07 +0200

    set default value for  enableLowercase in kafka test

--- cygnus-ngsi/src/test/java/com/telefonica/iot/cygnus/sinks/NGSIKafkaSinkTest.java
+++ cygnus-ngsi/src/test/java/com/telefonica/iot/cygnus/sinks/NGSIKafkaSinkTest.java
@@ -35,7 +35,7 @@ public class NGSIKafkaSinkTest {
     private final String servicePathSlash = "/";
     private final String entity = "entityId=entityType";
     private final String attribute = "attributeName";
-    private final boolean enableLowercase = true;
+    private final boolean enableLowercase = false;
     private String expectedTopic;
 
     
