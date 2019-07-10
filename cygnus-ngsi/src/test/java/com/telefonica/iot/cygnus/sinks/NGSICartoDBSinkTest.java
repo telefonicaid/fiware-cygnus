17ceceb0c36c350997a66824104c46d49569890b
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2019-07-10 10:19:04 +0200

    fix carto test

--- cygnus-ngsi/src/test/java/com/telefonica/iot/cygnus/sinks/NGSICartoDBSinkTest.java
+++ cygnus-ngsi/src/test/java/com/telefonica/iot/cygnus/sinks/NGSICartoDBSinkTest.java
@@ -1749,7 +1749,7 @@ public class NGSICartoDBSinkTest {
             String rows = aggregator.getRows();
             
             try {
-                assertTrue(rows.contains("40.3833,-3.7167"));
+                assertTrue(rows.contains("(double)40.3833, (double)-3.7167"));
                 System.out.println(getTestTraceHead("[CartoDBAggregator.aggregate]")
                         + "-  OK  - '" + rows + "' contains the coordinates '-3.7167, 40.3833' swapped");
             } catch (AssertionError e) {
