5c799c24c3546b114cd7eb81d8cfbc8391766d11
Ivan Aaron Hernandez Cruz <ivan.hernandez@atlantida.es>
2019-11-27 11:43:12 +0100

    typo fix

--- cygnus-ngsi/resources/ngsi-examples/NGSIv2/notification-json-multiple.sh
+++ cygnus-ngsi/resources/ngsi-examples/NGSIv2/notification-json-multiple.sh
@@ -37,7 +37,7 @@ else
    SERVICE_PATH=/
 fi
 
-curl $URL -v -s -S --header 'Content-Type: application/json; charset=utf-8' --header 'Accept: application/json' --header 'User-Agent: orion/0.10.0' --header "Fiware-Service: $SERVICE" --header "Fiware-ServicePath: $SERVICE_PATH" --header "ngsiv2-attrsformat: normalized"-d @- <<EOF
+curl $URL -v -s -S --header 'Content-Type: application/json; charset=utf-8' --header 'Accept: application/json' --header 'User-Agent: orion/0.10.0' --header "Fiware-Service: $SERVICE" --header "Fiware-ServicePath: $SERVICE_PATH" --header "ngsiv2-attrsformat: normalized" -d @- <<EOF
 {
   "subscriptionId" : "51c0ac9ed714fb3b37d7d5a8",
   "data" : [
