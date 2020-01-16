912f6eec957d03dd10609e492554c53d7ed1c6fb
Ivan Hernandez <ivan.h.cruz@gmail.com>
2020-01-16 15:52:31 +0100

    add generic classes

---
+++ cygnus-ngsi/src/main/java/com/telefonica/iot/cygnus/utils/NGSIGenericAggregator.java
@@ -0,0 +1,271 @@
+package com.telefonica.iot.cygnus.utils;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.telefonica.iot.cygnus.errors.CygnusBadConfiguration;
+import com.telefonica.iot.cygnus.interceptors.NGSIEvent;
+import com.telefonica.iot.cygnus.log.CygnusLogger;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.flume.Context;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+/**
+ * The type Ngsi generic aggregator.
+ */
+abstract class NGSIGenericAggregator {
+
+    // Logger
+    private static final CygnusLogger LOGGER = new CygnusLogger(NGSIGenericAggregator.class);
+
+    // Setup for casting to native persistance types
+    private boolean attrNativeTypes;
+    /**
+     * The Enable grouping.
+     */
+    protected boolean enableGrouping;
+    /**
+     * The Enable encoding.
+     */
+    protected boolean enableEncoding;
+    /**
+     * The Enable name mappings.
+     */
+    protected boolean enableNameMappings;
+    /**
+     * The Enable geo parse.
+     */
+    protected boolean enableGeoParse;
+
+    // Default value for attrNativeTypes
+    private static final String DEFAULT_ATTR_NATIVE_TYPES = "false";
+
+
+    /**
+     * Configure.
+     *
+     * @param context the context
+     */
+    public void configure(Context context) {
+        String attrNativeTypesStr = context.getString("attr_native_types", DEFAULT_ATTR_NATIVE_TYPES);
+        if (attrNativeTypesStr.equals("true") || attrNativeTypesStr.equals("false")) {
+            attrNativeTypes = Boolean.valueOf(attrNativeTypesStr);
+            LOGGER.debug("[" + getName() + "] Reading configuration (attr_native_types=" + attrNativeTypesStr + ")");
+        } else {
+            LOGGER.debug("[" + getName() + "] Invalid configuration (attr_native_types=" + attrNativeTypesStr + ") -- Must be 'true' or 'false' taking default false");
+        }
+    }
+
+    /**
+     * The Aggregation.
+     */
+// object containing the aggregated data
+    protected LinkedHashMap<String, ArrayList<JsonElement>> aggregation;
+
+    private String service;
+    private String servicePathForData;
+    private String servicePathForNaming;
+    private String entityForNaming;
+    private String entityType;
+    private String attribute;
+    private String dbName;
+    private String tableName;
+
+    /**
+     * Instantiates a new Ngsi generic aggregator.
+     *
+     * @param enableGrouping     the enable grouping
+     * @param enableNameMappings the enable name mappings
+     * @param enableEncoding     the enable encoding
+     * @param enableGeoParse     the enable geo parse
+     */
+    NGSIGenericAggregator(boolean enableGrouping, boolean enableNameMappings, boolean enableEncoding, boolean enableGeoParse) {
+        this.enableEncoding = enableEncoding;
+        this.enableNameMappings = enableNameMappings;
+        this.enableEncoding = enableEncoding;
+        this.enableGeoParse = enableGeoParse;
+        aggregation = new LinkedHashMap<>();
+    } // MySQLAggregator
+
+    /**
+     * Gets aggregation.
+     *
+     * @return the aggregation
+     */
+    protected LinkedHashMap<String, ArrayList<JsonElement>> getAggregation() {
+        return aggregation;
+    } //getAggregation
+
+    /**
+     * Gets service path for data.
+     *
+     * @return the service path for data
+     */
+    protected String getServicePathForData() {
+        return servicePathForData;
+    } //getServicePathForData
+
+    /**
+     * Gets db name.
+     *
+     * @param enableLowercase the enable lowercase
+     * @return the db name
+     */
+    public String getDbName(boolean enableLowercase) {
+        if (enableLowercase) {
+            return dbName.toLowerCase();
+        } else {
+            return dbName;
+        } // if else
+    } // getDbName
+
+    /**
+     * Gets table name.
+     *
+     * @param enableLowercase the enable lowercase
+     * @return the table name
+     */
+    public String getTableName(boolean enableLowercase) {
+        if (enableLowercase) {
+            return tableName.toLowerCase();
+        } else {
+            return tableName;
+        } // if else
+    } // getTableName
+
+    /**
+     * Gets string value for json element.
+     *
+     * @param value the value
+     * @return the string value for json element
+     */
+    public String getStringValueForJsonElement(JsonElement value) {
+        String stringValue;
+        if (attrNativeTypes) {
+            LOGGER.debug("[" + getName() + "] aggregation entry = "  + value.toString() );
+            if (value == null || value.isJsonNull()) {
+                stringValue = "NULL";
+            } else if (value.isJsonPrimitive()) {
+                if (value.getAsJsonPrimitive().isBoolean()) {
+                    stringValue = value.getAsString().toUpperCase();
+                } else if (value.getAsJsonPrimitive().isNumber()) {
+                    stringValue = value.getAsString();
+                }else {
+                    stringValue = "'" + value.getAsString() + "'";
+                }
+            } else {
+                stringValue = "'" + value.toString() + "'";
+            }
+        } else {
+            if (value.isJsonPrimitive()) {
+                stringValue = "'" + value.getAsString() + "'";
+            } else {
+                stringValue = "'" + value.toString() + "'";
+            }
+        }
+        return stringValue;
+    }
+
+    /**
+     * Gets values for insert.
+     *
+     * @return the values for insert
+     */
+    public String getValuesForInsert() {
+        String valuesForInsert = "";
+        int numEvents = aggregation.get(NGSIConstants.FIWARE_SERVICE_PATH).size();
+
+        for (int i = 0; i < numEvents; i++) {
+            if (i == 0) {
+                valuesForInsert += "(";
+            } else {
+                valuesForInsert += ",(";
+            } // if else
+            boolean first = true;
+            Iterator<String> it = aggregation.keySet().iterator();
+            while (it.hasNext()) {
+                String entry = (String) it.next();
+                ArrayList<JsonElement> values = (ArrayList<JsonElement>) aggregation.get(entry);
+                JsonElement value = values.get(i);
+                String stringValue = getStringValueForJsonElement(value);
+                if (first) {
+                    valuesForInsert += stringValue;
+                    first = false;
+                } else {
+                    valuesForInsert += "," + stringValue;
+                } // if else
+            } // while
+            valuesForInsert += ")";
+        } // for
+        return valuesForInsert;
+    } // getValuesForInsert
+
+    private String getName() {
+        return "NGSIUtils.GenericAggregator";
+    }
+
+    /**
+     * Gets fields for create.
+     *
+     * @return the fields for create
+     */
+    public String getFieldsForCreate() {
+        String fieldsForCreate = "(";
+        boolean first = true;
+        Iterator<String> it = aggregation.keySet().iterator();
+
+        while (it.hasNext()) {
+            if (first) {
+                fieldsForCreate += (String) it.next() + " text";
+                first = false;
+            } else {
+                fieldsForCreate += "," + (String) it.next() + " text";
+            } // if else
+        } // while
+
+        return fieldsForCreate + ")";
+    } // getFieldsForCreate
+
+    /**
+     * Gets fields for insert.
+     *
+     * @return the fields for insert
+     */
+    public String getFieldsForInsert() {
+        String fieldsForInsert = "(";
+        boolean first = true;
+        Iterator<String> it = aggregation.keySet().iterator();
+        while (it.hasNext()) {
+            if (first) {
+                fieldsForInsert += (String) it.next();
+                first = false;
+            } else {
+                fieldsForInsert += "," + (String) it.next();
+            } // if else
+        } // while
+        return fieldsForInsert + ")";
+    } // getFieldsForInsert
+
+    /**
+     * Initialize.
+     *
+     * @param event the event
+     * @throws CygnusBadConfiguration the cygnus bad configuration
+     */
+    public void initialize(NGSIEvent event) throws CygnusBadConfiguration {
+        service = event.getServiceForNaming(enableNameMappings);
+        servicePathForData = event.getServicePathForData();
+        servicePathForNaming = event.getServicePathForNaming(enableGrouping, enableNameMappings);
+        entityForNaming = event.getEntityForNaming(enableGrouping, enableNameMappings, enableEncoding);
+    } // initialize
+
+    /**
+     * Aggregate.
+     *
+     * @param cygnusEvent the cygnus event
+     */
+    public abstract void aggregate(NGSIEvent cygnusEvent);
+
+}
---
+++ cygnus-ngsi/src/main/java/com/telefonica/iot/cygnus/utils/NGSIGenericColumnAggregator.java
@@ -0,0 +1,136 @@
+package com.telefonica.iot.cygnus.utils;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.telefonica.iot.cygnus.containers.NotifyContextRequest;
+import com.telefonica.iot.cygnus.errors.CygnusBadConfiguration;
+import com.telefonica.iot.cygnus.interceptors.NGSIEvent;
+import com.telefonica.iot.cygnus.log.CygnusLogger;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+
+/**
+ * The type Ngsi generic column aggregator.
+ */
+public class NGSIGenericColumnAggregator extends NGSIGenericAggregator {
+
+    // Logger
+    private static final CygnusLogger LOGGER = new CygnusLogger(NGSIGenericAggregator.class);
+
+    private String entityType;
+    private String attribute;
+    private boolean enableGeoParse;
+    private boolean swapCoordinates;
+
+    /**
+     * Instantiates a new Ngsi generic column aggregator.
+     *
+     * @param enableGrouping     the enable grouping
+     * @param enableNameMappings the enable name mappings
+     * @param enableEncoding     the enable encoding
+     * @param enableGeoParse     the enable geo parse
+     */
+    public NGSIGenericColumnAggregator(boolean enableGrouping, boolean enableNameMappings, boolean enableEncoding, boolean enableGeoParse) {
+        super(enableGrouping, enableNameMappings, enableEncoding, enableGeoParse);
+        this.enableGeoParse = enableGeoParse;
+    }
+
+    @Override
+    public void initialize(NGSIEvent event) throws CygnusBadConfiguration {
+        super.initialize(event);
+        entityType = event.getEntityTypeForNaming(enableGrouping, enableNameMappings);
+        attribute = event.getAttributeForNaming(enableNameMappings);
+        // TBD: possible option for postgisSink
+        swapCoordinates = false;
+        // particular initialization
+        LinkedHashMap<String, ArrayList<JsonElement>> aggregation = getAggregation();
+        aggregation.put(NGSIConstants.RECV_TIME, new ArrayList<JsonElement>());
+        aggregation.put(NGSIConstants.FIWARE_SERVICE_PATH, new ArrayList<JsonElement>());
+        aggregation.put(NGSIConstants.ENTITY_ID, new ArrayList<JsonElement>());
+        aggregation.put(NGSIConstants.ENTITY_TYPE, new ArrayList<JsonElement>());
+        // iterate on all this context element attributes, if there are attributes
+        ArrayList<NotifyContextRequest.ContextAttribute> contextAttributes = event.getContextElement().getAttributes();
+        if (contextAttributes == null || contextAttributes.isEmpty()) {
+            return;
+        } // if
+        for (NotifyContextRequest.ContextAttribute contextAttribute : contextAttributes) {
+            String attrName = contextAttribute.getName();
+            aggregation.put(attrName, new ArrayList<JsonElement>());
+            aggregation.put(attrName + "_md", new ArrayList<JsonElement>());
+        } // for
+    } // initialize
+
+    @Override
+    public void aggregate(NGSIEvent event) {
+        // Number of previous values
+        int numPreviousValues = getAggregation().get(NGSIConstants.FIWARE_SERVICE_PATH).size();
+        // Get the event headers
+        long recvTimeTs = event.getRecvTimeTs();
+        String recvTime = CommonUtils.getHumanReadable(recvTimeTs, false);
+        // get the event body
+        NotifyContextRequest.ContextElement contextElement = event.getContextElement();
+        String entityId = contextElement.getId();
+        String entityType = contextElement.getType();
+        LOGGER.debug("[" + getName() + "] Processing context element (id=" + entityId + ", type=" + entityType + ")");
+        // Iterate on all this context element attributes, if there are attributes
+        ArrayList<NotifyContextRequest.ContextAttribute> contextAttributes = contextElement.getAttributes();
+        if (contextAttributes == null || contextAttributes.isEmpty()) {
+            LOGGER.warn("No attributes within the notified entity, nothing is done (id=" + entityId
+                    + ", type=" + entityType + ")");
+            return;
+        } // if
+        LinkedHashMap<String, ArrayList<JsonElement>> aggregation = getAggregation();
+        aggregation.get(NGSIConstants.RECV_TIME).add(new JsonPrimitive(recvTime));
+        aggregation.get(NGSIConstants.FIWARE_SERVICE_PATH).add(new JsonPrimitive(getServicePathForData()));
+        aggregation.get(NGSIConstants.ENTITY_ID).add(new JsonPrimitive(entityId));
+        aggregation.get(NGSIConstants.ENTITY_TYPE).add(new JsonPrimitive(entityType));
+        for (NotifyContextRequest.ContextAttribute contextAttribute : contextAttributes) {
+            String attrName = contextAttribute.getName();
+            String attrType = contextAttribute.getType();
+            JsonElement attrValue = contextAttribute.getValue();
+            String attrMetadata = contextAttribute.getContextMetadata();
+            LOGGER.debug("[" + getName() + "] Processing context attribute (name=" + attrName + ", type=" + attrType + ")");
+            if (enableGeoParse && (attrType.equals("geo:json") || attrType.equals("geo:point"))) {
+                try {
+                    //Process geometry if applyes
+                    ImmutablePair<String, Boolean> location = NGSIUtils.getGeometry(attrValue.toString(), attrType, attrMetadata, swapCoordinates);
+                    if (location.right) {
+                        LOGGER.debug("location=" + location.getLeft());
+                        attrValue = new JsonPrimitive(location.getLeft());
+                    }
+                } catch (Exception e) {
+                    LOGGER.error("[" + getName() + "] Processing context attribute (name=" + attrValue.toString());
+                }
+            }
+            // Check if the attribute already exists in the form of 2 columns (one for metadata); if not existing,
+            // add an empty value for all previous rows
+            if (aggregation.containsKey(attrName)) {
+                aggregation.get(attrName).add(attrValue);
+                aggregation.get(attrName + "_md").add(new JsonPrimitive(attrMetadata));
+            } else {
+                ArrayList<JsonElement> values = new ArrayList<JsonElement>(Collections.nCopies(numPreviousValues, null));
+                values.add(attrValue);
+                aggregation.put(attrName, values);
+                ArrayList<JsonElement> valuesMd = new ArrayList<JsonElement>(Collections.nCopies(numPreviousValues, null));
+                valuesMd.add(new JsonPrimitive(attrMetadata));
+                aggregation.put(attrName + "_md", valuesMd);
+            } // if else
+        } // for
+        // Iterate on all the aggregations, checking for not updated attributes; add an empty value if missing
+        for (String key : aggregation.keySet()) {
+            ArrayList<JsonElement> values = aggregation.get(key);
+            if (values.size() == numPreviousValues) {
+                values.add(null);
+            } // if
+        } // for
+    }
+
+    private String getName() {
+        return "NGSIUtils.GenericColumnAggregator";
+    }
+
+
+}
