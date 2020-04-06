5ca1bfdf04cfaba6cf892f4cb617d648de225624
David Martínez <dmg895@alumnos.unican.es>
2020-04-06 11:59:19 +0200

    Create ArcgisBaseTest.java

---
+++ cygnus-arcgis/src/test/java/es/santander/smartcity/ArcgisRestUtils/ArcgisBaseTest.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package es.santander.smartcity.ArcgisRestUtils;
+
+/**
+ * @author dmartinez
+ *
+ */
+public interface ArcgisBaseTest {
+
+    public static final String PORTAL_PASSWORD = "************";
+    public static final String PORTAL_USER = "***************";
+    public static final String PORTAL_FEATURETABLE_URL = "***************";
+    public static final String PORTAL_PUBLIC_FEATURETABLE_URL = "***************";
+    public static final String PORTAL_GENERATE_TOKEN_URL = "***************";
+
+    public static final String ONLINE_PASSWORD = "***************";
+    public static final String ONLINE_USER = "***************";
+    public static final String ONLINE_GENERATE_TOKEN_URL = "***************";
+    public static final String ONLINE_FEATURETABLE_URL = "***************";
+
+    public boolean testPortal = false;
+
+    /**
+     * 
+     * @return
+     */
+    public static String getUser() {
+        if (testPortal) {
+            return PORTAL_USER;
+        } else {
+            return ONLINE_USER;
+        }
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static String getPassword() {
+        if (testPortal) {
+            return PORTAL_PASSWORD;
+        } else {
+            return ONLINE_PASSWORD;
+        }
+    }
+
+    public static String getGenerateTokenUrl() {
+        if (testPortal) {
+            return PORTAL_GENERATE_TOKEN_URL;
+        } else {
+            return ONLINE_GENERATE_TOKEN_URL;
+        }
+    }
+
+    public static String getFeatureUrl() {
+        if (testPortal) {
+            return PORTAL_FEATURETABLE_URL;
+        } else {
+            return ONLINE_FEATURETABLE_URL;
+        }
+    }
+
+}
