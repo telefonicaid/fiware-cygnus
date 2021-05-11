1cb3be667160743c5b5ca17dd08aa70755e881b5
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2021-05-11 12:17:14 +0200

    Update CommonUtils.java

--- cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonUtils.java
+++ cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonUtils.java
@@ -45,8 +45,8 @@ import kafka.common.OffsetOutOfRangeException;
 import kafka.tools.KafkaMigrationTool;
 import org.I0Itec.zkclient.exception.ZkNoNodeException;
 import org.apache.flume.interceptor.RegexExtractorInterceptorMillisSerializer;
-//import org.apache.hive.jdbc.HivePreparedStatement;
-import org.apache.hadoop.hive.ql.exec.AbstractMapJoinOperator;
+import org.apache.hive.jdbc.HivePreparedStatement;
+//import org.apache.hadoop.hive.ql.exec.AbstractMapJoinOperator;
 import org.apache.hadoop.metrics.spi.AbstractMetricsContext;
 import org.apache.http.impl.DefaultBHttpServerConnection;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
@@ -462,12 +462,12 @@ public final class CommonUtils {
         LOGGER.debug("Loading hadoop-core from " + myClassURL.toExternalForm());
         
         // trace the file containing the hive-exec library
-        myClassURL = AbstractMapJoinOperator.class.getProtectionDomain().getCodeSource().getLocation();
-        LOGGER.debug("Loading hive-exec from " + myClassURL.toExternalForm());
+        //myClassURL = AbstractMapJoinOperator.class.getProtectionDomain().getCodeSource().getLocation();
+        //LOGGER.debug("Loading hive-exec from " + myClassURL.toExternalForm());
         
         // trace the file containing the hive-jdbc library
-        //myClassURL = HivePreparedStatement.class.getProtectionDomain().getCodeSource().getLocation();
-        //LOGGER.debug("Loading hive-jdbc from " + myClassURL.toExternalForm());
+        myClassURL = HivePreparedStatement.class.getProtectionDomain().getCodeSource().getLocation();
+        LOGGER.debug("Loading hive-jdbc from " + myClassURL.toExternalForm());
         
         // trace the file containing the mongodb-driver library
         myClassURL = AsyncReadWriteBinding.class.getProtectionDomain().getCodeSource().getLocation();
