8fd142322c653d1956be1067b0e3ab6452afb00d
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2021-12-14 11:07:33 +0100

    delete unused import

--- cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonUtils.java
+++ cygnus-common/src/main/java/com/telefonica/iot/cygnus/utils/CommonUtils.java
@@ -50,7 +50,6 @@ import org.apache.hadoop.metrics.spi.AbstractMetricsContext;
 import org.apache.http.impl.DefaultBHttpServerConnection;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.logging.log4j.core.pattern.SequenceNumberPatternConverter;
-import org.codehaus.groovy.control.ErrorCollector;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
